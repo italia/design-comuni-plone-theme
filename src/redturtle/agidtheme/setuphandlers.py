8565fd5891d5ce25c7f51a704d5d63d59434b06f
Federica D'Elia <federica@Federica.local>
2017-06-13 11:50:16 +0200

    aggiunto stile portlet

--- src/redturtle/agidtheme/setuphandlers.py
+++ src/redturtle/agidtheme/setuphandlers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
+#from Products.CMFCore.utils import getToolByName
 from zope.interface import implementer
 
 
@@ -21,3 +22,22 @@ def post_install(context):
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+
+# def setupVarious(context):
+
+#     if context.readDataFile('redturtle.agidtheme_various.txt') is None:
+#         return
+
+#     portal = context.getSite()
+#     addPropertySheet(portal)
+
+# def addPropertySheet(portal):
+#     portal_properties = getToolByName(portal, 'portal_properties')
+#     STYLES = ("portletStaticNavigation|stile menu di navigazione",)
+#     rer_staticportlet_properties = getattr(portal_properties, 'rer_staticportlet_properties', None)
+#     if not rer_staticportlet_properties:
+#         portal_properties.addPropertySheet(id='rer_staticportlet_properties', title='RER Advanced static portlet properties')
+#         portal.plone_log("Added RER Advanced static portlet properties property-sheet")
+#         rer_staticportlet_properties = getattr(portal_properties, 'rer_staticportlet_properties', None)
+#     if not rer_staticportlet_properties.hasProperty('portlet_styles_menu'):
+#         rer_staticportlet_properties.manage_addProperty(id='portlet_styles_menu', value=STYLES, type='lines')
