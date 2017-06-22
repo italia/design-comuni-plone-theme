1b45f682bd397af10d03dd08682f7c9c4992d78d
nicolazambello <nicola.zambello@redturtle.it>
2017-06-22 11:53:53 +0200

    homepage collection aree-tematiche fix stile tendina

--- src/redturtle/agidtheme/browser/homepage_smartlink_collection.py
+++ src/redturtle/agidtheme/browser/homepage_smartlink_collection.py
@@ -8,4 +8,4 @@ from collective.tiles.collection import _
 class BaseView(BrowserView):
     implements(ICollectionTileRenderer)
 
-    display_name = _("Homepage SmartLink collection")
+    display_name = _("Collezione SmartLink aree tematiche")
