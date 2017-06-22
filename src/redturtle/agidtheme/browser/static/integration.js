0999773bb95a1b8f11661c578bbd9f5ce568fb3f
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-06-22 16:02:28 +0200

    add bundle for theme javascripts + fix cache versioning for css resources

---
+++ src/redturtle/agidtheme/browser/static/integration.js
@@ -0,0 +1,21 @@
+require([
+  'jquery',
+], function ($) {
+  'use strict';
+  $(document).ready(function () {
+    // return-to-top arrow
+    $(window).scroll(function () {
+      if ($(this).scrollTop() >= 50) {
+        $('#return-to-top').fadeIn(200);
+      } else {
+        $('#return-to-top').fadeOut(200);
+      }
+    });
+
+    $('#return-to-top').click(function () {
+      $('body,html').animate({
+          scrollTop: 0,
+        }, 500);
+    });
+  });
+});
