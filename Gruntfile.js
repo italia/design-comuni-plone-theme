5220253c17213975a0784f2138c441313b80f0fa
nzambello <nicola.zambello@redturtle.it>
2017-09-04 08:42:02 +0200

    update autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,8 +51,8 @@ module.exports = function (grunt) {
 			options: {
 				map: true,
 				processors: [
-					require('autoprefixer')({
-						browsers: ['last 2 versions'],
+			    require('autoprefixer')({
+            browsers: ['last 2 versions', 'ie >= 11'],
 					}),
 				],
 			},
