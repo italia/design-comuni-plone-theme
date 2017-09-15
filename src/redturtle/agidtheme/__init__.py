cfb570ecdc779ba529e705d6dda57a39655cbb97
nzambello <nicola.zambello@redturtle.it>
2017-09-15 17:53:39 +0200

    fix import stili css tiles statiche -> registry.xml

--- src/redturtle/agidtheme/__init__.py
+++ src/redturtle/agidtheme/__init__.py
@@ -2,5 +2,8 @@
 """Init and utils."""
 
 from zope.i18nmessageid import MessageFactory
+import logging
 
 _ = MessageFactory('redturtle.agidtheme')
+
+logger = logging.getLogger('redturtle.agidtheme')
---
+++ src/redturtle/agidtheme/upgrades/upgrades.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from redturtle.agidtheme import logger
+from plone import api
+
+DEFAULT_PROFILE = 'profile-redturtle.agidtheme:default'
+
+
+def import_registry(registry_id, dependencies=False):
+    setup_tool = api.portal.get_tool('portal_setup')
+    setup_tool.runImportStepFromProfile(DEFAULT_PROFILE, registry_id,
+                                        run_dependencies=dependencies)
+
+
+def import_css_registry(context):
+    'Import CSS registry configuration'
+    logger.info('Importing CSS registry configuration for ' +
+                'redturtle.agidtheme')
+    import_registry('cssregistry')
+
+
+def import_js_registry(context):
+    'Import js registry configuration'
+    logger.info('Importing js registry configuration for ' +
+                'redturtle.agidtheme')
+    import_registry('jsregistry')
+
+
+def import_actions_registry(context):
+    'Import actions registry configuration'
+    logger.info('Importing actions registry configuration for ' +
+                'redturtle.agidtheme')
+    import_registry('actions')
+
+
+def import_types_registry(context):
+    'Import types registry configuration'
+    logger.info('Importing types registry configuration for ' +
+                'redturtle.agidtheme')
+    import_registry('typeinfo')
+
+
+def import_viewlets_registry(context):
+    'Import viewlets registry configuration'
+    logger.info('Importing viewlets registry configuration for ' +
+                'redturtle.agidtheme')
+    import_registry('viewlets')
+
+
+def import_properties_registry(context):
+    'Import properties registry configuration'
+    logger.info('Importing properties registry configuration for ' +
+                'redturtle.agidtheme')
+    import_registry('propertiestool')
+
+
+def import_records_registry(context):
+    'Import records and settings'
+    logger.info('Importing records and settings configuration for' +
+                'redturtle.agidtheme')
+    import_registry('plone.app.registry')
