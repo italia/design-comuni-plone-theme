acde462b11667e7bde17563a791698b9ba369c0d
daniele-andreotti <daniele.andreotti@redturtle.it>
2017-09-28 10:12:55 +0200

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='redturtle.agidtheme',
-    version='1.0.2',
+    version='1.0.3.dev0',
     description="RedTurtle Agid Theme",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
