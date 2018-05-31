74adec72fe0c704bcd8e5090d20472d8188a1017
nzambello <nicola.zambello@redturtle.it>
2018-05-31 10:22:18 +0200

    Back to development: 2.1.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='redturtle.agidtheme',
-    version='2.1.4',
+    version='2.1.5.dev0',
     description="RedTurtle Agid Theme",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
