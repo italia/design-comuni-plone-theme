2fb61e8e4b58cf559701a35460001c4a31b5a055
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-01-09 10:50:06 +0100

    Back to development: 3.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='design.plone.theme',
-    version='3.1.0',
+    version='3.1.1.dev0',
     description="Theme for Italia Design System",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
