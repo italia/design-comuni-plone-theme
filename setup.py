34d5de39257f981398ff0e765e1038bb45779e3d
nzambello <nzambello@protonmail.com>
2019-01-22 10:24:08 +0100

    Back to development: 3.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='design.plone.theme',
-    version='3.1.1',
+    version='3.1.2.dev0',
     description="Theme for Italia Design System",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
