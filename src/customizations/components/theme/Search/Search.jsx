0b9df470be5d4fdaa9d2e363496a44ed6e28df36
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-04-09 10:44:00 +0200

    added file for search results customization

---
+++ src/customizations/components/theme/Search/Search.jsx
@@ -0,0 +1,298 @@
+/**
+ * Search component.
+ * @module components/theme/Search/Search
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { Link } from 'react-router-dom';
+import { asyncConnect } from 'redux-connect';
+import { FormattedMessage } from 'react-intl';
+import { Portal } from 'react-portal';
+import { Container, Pagination } from 'semantic-ui-react';
+import qs from 'query-string';
+
+import { settings } from '~/config';
+import { Helmet } from '@plone/volto/helpers';
+import { searchContent } from '@plone/volto/actions';
+import { SearchTags, Toolbar, Icon } from '@plone/volto/components';
+
+import paginationLeftSVG from '@plone/volto/icons/left-key.svg';
+import paginationRightSVG from '@plone/volto/icons/right-key.svg';
+
+const toSearchOptions = (searchableText, subject, path) => {
+  return {
+    ...(searchableText && { SearchableText: searchableText }),
+    ...(subject && {
+      Subject: subject,
+    }),
+    ...(path && {
+      path: path,
+    }),
+  };
+};
+
+/**
+ * Search class.
+ * @class SearchComponent
+ * @extends Component
+ */
+class Search extends Component {
+  /**
+   * Property types.
+   * @property {Object} propTypes Property types.
+   * @static
+   */
+  static propTypes = {
+    searchContent: PropTypes.func.isRequired,
+    searchableText: PropTypes.string,
+    subject: PropTypes.string,
+    path: PropTypes.string,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        '@id': PropTypes.string,
+        '@type': PropTypes.string,
+        title: PropTypes.string,
+        description: PropTypes.string,
+      }),
+    ),
+    pathname: PropTypes.string.isRequired,
+  };
+
+  /**
+   * Default properties.
+   * @property {Object} defaultProps Default properties.
+   * @static
+   */
+  static defaultProps = {
+    items: [],
+    searchableText: null,
+    subject: null,
+    path: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { currentPage: 1 };
+  }
+
+  /**
+   * Component will mount
+   * @method componentWillMount
+   * @returns {undefined}
+   */
+  UNSAFE_componentWillMount() {
+    this.doSearch(
+      this.props.searchableText,
+      this.props.subject,
+      this.props.path,
+    );
+  }
+
+  /**
+   * Component will receive props
+   * @method componentWillReceiveProps
+   * @param {Object} nextProps Next properties
+   * @returns {undefined}
+   */
+  UNSAFE_componentWillReceiveProps = nextProps => {
+    if (
+      nextProps.searchableText !== this.props.searchableText ||
+      nextProps.subject !== this.props.subject
+    ) {
+      this.doSearch(
+        nextProps.searchableText,
+        nextProps.subject,
+        this.props.path,
+      );
+    }
+  };
+
+  /**
+   * Search based on the given searchableText, subject and path.
+   * @method doSearch
+   * @param {string} searchableText The searchable text string
+   * @param {string} subject The subject (tag)
+   * @param {string} path The path to restrict the search to
+   * @returns {undefined}
+   */
+
+  doSearch = (searchableText, subject, path) => {
+    this.setState({ currentPage: 1 });
+    this.props.searchContent(
+      '',
+      toSearchOptions(searchableText, subject, path),
+    );
+  };
+
+  handleQueryPaginationChange = (e, { activePage }) => {
+    window.scrollTo(0, 0);
+    this.setState({ currentPage: activePage }, () => {
+      const options = toSearchOptions(
+        qs.parse(this.props.location.search).SearchableText,
+        qs.parse(this.props.location.search).Subject,
+        qs.parse(this.props.location.search).path,
+      );
+
+      this.props.searchContent('', {
+        ...options,
+        b_start: (this.state.currentPage - 1) * settings.defaultPageSize,
+      });
+    });
+  };
+
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    return (
+      <Container id="page-search">
+        <Helmet title="Search" />
+        <div className="container">
+          <article id="content">
+            <header>
+              <h1 className="documentFirstHeading">
+                {this.props.searchableText ? (
+                  <FormattedMessage
+                    id="Search results for {term}"
+                    defaultMessage="Search results for {term}"
+                    values={{
+                      term: <q>{this.props.searchableText}</q>,
+                    }}
+                  />
+                ) : (
+                  <FormattedMessage
+                    id="Search results"
+                    defaultMessage="Search results"
+                  />
+                )}
+              </h1>
+
+              <SearchTags />
+
+              {this.props.search?.items_total && (
+                <div className="items_total">
+                  {this.props.search.items_total}{' '}
+                  <FormattedMessage
+                    id="results found"
+                    defaultMessage="results"
+                  />
+                </div>
+              )}
+            </header>
+            <section id="content-core">
+              {this.props.items.map(item => (
+                <article className="tileItem" key={item['@id']}>
+                  <h2 className="tileHeadline">
+                    <Link
+                      to={item['@id']}
+                      className="summary url"
+                      title={item['@type']}
+                    >
+                      {item.title}
+                    </Link>
+                  </h2>
+                  {item.description && (
+                    <div className="tileBody">
+                      <span className="description">{item.description}</span>
+                    </div>
+                  )}
+                  <div className="tileFooter">
+                    <Link to={item['@id']}>
+                      <FormattedMessage
+                        id="Read More…"
+                        defaultMessage="Read More…"
+                      />
+                    </Link>
+                  </div>
+                  <div className="visualClear" />
+                </article>
+              ))}
+
+              {this.props.search?.batching && (
+                <div className="search-footer">
+                  <Pagination
+                    activePage={this.state.currentPage}
+                    totalPages={Math.ceil(
+                      this.props.search.items_total / settings.defaultPageSize,
+                    )}
+                    onPageChange={this.handleQueryPaginationChange}
+                    firstItem={null}
+                    lastItem={null}
+                    prevItem={{
+                      content: <Icon name={paginationLeftSVG} size="18px" />,
+                      icon: true,
+                      'aria-disabled': !this.props.search.batching.prev,
+                      className: !this.props.search.batching.prev
+                        ? 'disabled'
+                        : null,
+                    }}
+                    nextItem={{
+                      content: <Icon name={paginationRightSVG} size="18px" />,
+                      icon: true,
+                      'aria-disabled': !this.props.search.batching.next,
+                      className: !this.props.search.batching.next
+                        ? 'disabled'
+                        : null,
+                    }}
+                  />
+                </div>
+              )}
+            </section>
+          </article>
+        </div>
+        <Portal node={__CLIENT__ && document.getElementById('toolbar')}>
+          <Toolbar
+            pathname={this.props.pathname}
+            hideDefaultViewButtons
+            inner={<span />}
+          />
+        </Portal>
+      </Container>
+    );
+  }
+}
+
+export const __test__ = connect(
+  (state, props) => ({
+    items: state.search.items,
+    searchableText: qs.parse(props.location.search).SearchableText,
+    subject: qs.parse(props.location.search).Subject,
+    path: qs.parse(props.location.search).path,
+    pathname: props.location.pathname,
+  }),
+  { searchContent },
+)(Search);
+
+export default compose(
+  connect(
+    (state, props) => ({
+      items: state.search.items,
+      searchableText: qs.parse(props.location.search).SearchableText,
+      subject: qs.parse(props.location.search).Subject,
+      path: qs.parse(props.location.search).path,
+      pathname: props.location.pathname,
+    }),
+    { searchContent },
+  ),
+  asyncConnect([
+    {
+      key: 'search',
+      promise: ({ location, store: { dispatch } }) =>
+        dispatch(
+          searchContent(
+            '',
+            toSearchOptions(
+              qs.parse(location.search).SearchableText,
+              qs.parse(location.search).Subject,
+              qs.parse(location.search).path,
+            ),
+          ),
+        ),
+    },
+  ]),
+)(Search);
