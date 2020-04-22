03902b19799f9389847b0e256cc0ceac04dadcb4
Nicola Zambello <nzambello@protonmail.com>
2020-04-22 11:10:21 +0200

    fix search filters reducer

--- src/reducers/searchFiltersReducer.js
+++ src/reducers/searchFiltersReducer.js
@@ -1,26 +1,4 @@
 import { UPDATE_SEARCH_FILTERS } from '../actions/updateSearchFilters';
 
-export const searchFiltersReducer = (state = {}, action = {}) => {
-  switch (action.type) {
-    case `${UPDATE_SEARCH_FILTERS}_PENDING`:
-      return {
-        ...state,
-        loadingResults: true,
-      };
-    case `${UPDATE_SEARCH_FILTERS}_SUCCESS`:
-      return {
-        ...state,
-        result: action.content,
-        loadingResults: false,
-      };
-    case `${UPDATE_SEARCH_FILTERS}_FAIL`:
-      return {
-        ...state,
-        error: action.error,
-        hasError: true,
-        loadingResults: false,
-      };
-    default:
-      return state;
-  }
-};
+export const searchFiltersReducer = (state = {}, action = {}) =>
+  action.type === UPDATE_SEARCH_FILTERS ? action.content : state;
