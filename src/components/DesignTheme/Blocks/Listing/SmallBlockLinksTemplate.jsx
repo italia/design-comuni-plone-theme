f2cee641bfa6a52e667d4f82da9177104f42e5c3
Gabriele Damiani <gabriele.damiani@redturtle.it>
2020-05-27 13:03:56 +0200

    added smallBalockLinksTemplate

---
+++ src/components/DesignTheme/Blocks/Listing/SmallBlockLinksTemplate.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col } from 'design-react-kit/dist/design-react-kit';
+
+const SmallBlockLinksTemplate = ({ items, title }) => {
+  return (
+    <div className="small-block-links">
+      <div className="title">{title && <h3>{title}</h3>}</div>
+      <Row className="items">
+        {items.map((item, index) => (
+          <Col md="2" key={item['@id']} className="col-item">
+            {item.image && (
+              <div className="center-image-card">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={`${item['remoteUrl']}`}
+                >
+                  <img
+                    className="listing-image"
+                    alt="imagealt"
+                    src={item.image.scales.preview.download}
+                    title={item.title}
+                  />
+                </a>
+              </div>
+            )}
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+SmallBlockLinksTemplate.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.any).isRequired,
+  linkMore: PropTypes.any,
+  isEditMode: PropTypes.bool,
+  title: PropTypes.string,
+};
+
+export default SmallBlockLinksTemplate;
