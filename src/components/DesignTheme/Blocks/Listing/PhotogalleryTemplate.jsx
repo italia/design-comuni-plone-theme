c7a4560e29b486a4adbd2e635c54d2779eb10945
Gabriele Damiani <gabriele.damiani@redturtle.it>
2020-05-28 10:25:39 +0200

    priva versione photogallery

---
+++ src/components/DesignTheme/Blocks/Listing/PhotogalleryTemplate.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Row, Col } from 'design-react-kit/dist/design-react-kit';
+
+const PhotogalleryTemplate = ({ items, title }) => {
+  return (
+    <div className="small-block-links">
+      <div className="title">{<h3>PhotoGallery</h3>}</div>
+      <Row className="items">
+        {items.map((item, index) => (
+          <Col md="4" key={item['@id']} className="col-item">
+            <div style={{padding: "14px"}}>
+              {item.image && <img src={item.image.scales.preview.download} />}
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+PhotogalleryTemplate.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.any).isRequired,
+  linkMore: PropTypes.any,
+  isEditMode: PropTypes.bool,
+  title: PropTypes.string,
+};
+
+export default PhotogalleryTemplate;
