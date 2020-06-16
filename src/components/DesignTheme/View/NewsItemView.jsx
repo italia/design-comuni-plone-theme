8034adce9be3a778cb61ea961ce813607ae81f86
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-06-16 17:45:30 +0200

    hide CuredBy if no people or organization units are selected

--- src/components/DesignTheme/View/NewsItemView.jsx
+++ src/components/DesignTheme/View/NewsItemView.jsx
@@ -94,7 +94,8 @@ const NewsItemView = ({ content, location }) => {
               />
             )}
             {((content.a_cura_di && content.a_cura_di.length > 0) ||
-              content.a_cura_di_persone) && (
+              (content.a_cura_di_persone &&
+                content.a_cura_di_persone.length > 0)) && (
               <CuredBy
                 office={content.a_cura_di ? content.a_cura_di[0] : null}
                 people={content.a_cura_di_persone}
