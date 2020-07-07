55b9abd3c09c907298dd185bad1290a965e04538
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-07-07 15:00:12 +0200

    sidemenu refactor and fix

--- src/components/DesignTheme/View/Commons/SideMenu.jsx
+++ src/components/DesignTheme/View/Commons/SideMenu.jsx
@@ -1,6 +1,6 @@
 import { defineMessages, useIntl } from 'react-intl';
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { throttle } from 'lodash';
 
 const messages = defineMessages({
   index: {
@@ -36,25 +36,6 @@ const extractHeaders = (elements, intl) => {
         });
       }
     }
-
-    /*if (item.id === 'text-body') {
-      headers.push({
-        id: item.id,
-        title: intl.formatMessage(messages.contenuto),
-        item: item,
-      });
-    }
-
-    let item_headers = item.querySelectorAll('h1, h2, h3, h4, h5, h6');
-    item_headers.forEach((elem, index) => {
-      if (!elem.classList.contains('no-toc')) {
-        headers.push({
-          id: elem.getAttribute('id'),
-          title: elem.textContent,
-          item: elem,
-        });
-      }
-    });*/
   }
 
   return headers;
@@ -66,9 +47,9 @@ const extractHeaders = (elements, intl) => {
  * @params {object} content: Content object.
  * @returns {string} Markup of the component.
  */
-const SideMenu = ({ data, documentBody }) => {
+const SideMenu = ({ data }) => {
   const intl = useIntl();
-  //let history = useHistory();
+
   const [headers, _setHeaders] = useState([]);
   const headersRef = React.useRef(headers);
   const setHeaders = (data) => {
@@ -76,7 +57,7 @@ const SideMenu = ({ data, documentBody }) => {
     _setHeaders(data);
   };
 
-  const [activeSection, _setActiveSection] = useState();
+  const [activeSection, _setActiveSection] = useState(null);
   const activeSectionRef = React.useRef(activeSection);
   const setActiveSection = (data) => {
     activeSectionRef.current = data;
@@ -86,14 +67,22 @@ const SideMenu = ({ data, documentBody }) => {
 
   useEffect(() => {
     if (data?.children) {
-      setHeaders(extractHeaders(data.children, intl));
+      let extractedHeaders = extractHeaders(data.children, intl);
+      setHeaders(extractedHeaders);
+      setActiveSection(extractedHeaders[0].id);
     }
-
-    window.addEventListener('scroll', handleScroll);
   }, [data]);
 
-  const handleScroll = () => {
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = throttle(() => {
     let scrollDown = window.scrollY > windowScrollY;
+
     setWindowScrollY(window.scrollY);
     let scrollOffset = (scrollDown ? 0.15 : 0.85) * window.innerHeight;
     let headersHeights = headersRef.current
@@ -105,18 +94,26 @@ const SideMenu = ({ data, documentBody }) => {
         };
       })
       .filter((section) => section.top <= scrollOffset);
+
     if (headersHeights.length > 0) {
       let section = headersHeights.reduce(
         (prev, curr) => (prev.top > curr.top ? prev : curr),
         headersRef.current[0],
       );
 
-      console.log('activeSectionbyHeight', section.id);
       if (section.id !== activeSectionRef.current) {
-        console.log('setactive section', section.id);
         setActiveSection(section.id);
       }
     }
+  }, 100);
+
+  const handleClickAnchor = (id) => (e) => {
+    e.preventDefault();
+
+    document.getElementById(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
   };
 
   return headers?.length > 0 ? (
@@ -140,12 +137,6 @@ const SideMenu = ({ data, documentBody }) => {
               <span className="it-close"></span>Chiudi
             </button>
           </div>
-          {/* <a className="it-back-button" href="#">
-            <svg className="icon icon-sm icon-primary align-top">
-              <use xlink:href="{{ site.baseurl }}/assets/bootstrap-italia/dist/svg/sprite.svg#it-chevron-left"></use>
-            </svg>
-            <span>Torna indietro</span>
-          </a> */}
           <div className="menu-wrapper">
             <div className="link-list-wrapper menu-link-list">
               <h3 className="no-toc">{intl.formatMessage(messages.index)}</h3>
@@ -156,16 +147,8 @@ const SideMenu = ({ data, documentBody }) => {
                       className={`nav-link ${
                         item.id === activeSection && 'active'
                       }`}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setActiveSection(item.id);
-                        var anchorTarget = document.getElementById(item.id);
-                        anchorTarget.scrollIntoView({
-                          behavior: 'smooth',
-                          block: 'start',
-                        });
-                        //       history.push('#' + item.id);
-                      }}
+                      href={`#${item.id}`}
+                      onClick={handleClickAnchor(item.id)}
                     >
                       <span>{item.title}</span>
                     </a>
