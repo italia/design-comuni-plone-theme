805d4d6a661c35f2921610652658d2303140fe0f
DamiDev <dami.developer@gmail.com>
2020-07-21 15:42:37 +0200

    aggiunto bottone altro link

--- src/components/DesignTheme/Blocks/Listing/ArgumentsTemplate.jsx
+++ src/components/DesignTheme/Blocks/Listing/ArgumentsTemplate.jsx
@@ -6,23 +6,25 @@ import {
   CardTitle,
   CardCategory,
   CardText,
+  Button,
 } from 'design-react-kit/dist/design-react-kit';
 import { flattenToAppURL } from '@plone/volto/helpers';
 import { Link } from 'react-router-dom';
 import cx from 'classnames';
 
-const ArgumentsTemplate = ({ items, isEditMode }) => {
-
+const ArgumentsTemplate = ({ items, isEditMode, linkMore }) => {
+  console.log(items)
   const getIcon = (service) => {
+    console.log(service)
     switch (service) {
       case 'Servizio':
         return 'it-settings'
-      case 'Amministrazione':
+      case 'UnitaOrganizzativa':
         return 'it-pa'
       case 'Documento':
         return 'it-file'
       default:
-        return '';
+        return 'UnitaOrganizzativa';
     }
   }
   return (
@@ -31,7 +33,7 @@ const ArgumentsTemplate = ({ items, isEditMode }) => {
         'public-ui': isEditMode,
       })}
     >
-      <div className="container">
+      <div className="container mb-3">
         {items.map((item, index) => (
           <Card
             className={cx('listing-item card-bg')}
@@ -53,6 +55,18 @@ const ArgumentsTemplate = ({ items, isEditMode }) => {
           </Card>
         ))}
       </div>
+      {linkMore?.href && (
+        <div className="link-button">
+          <Button
+            className="view-all"
+            icon={false}
+            tag="button"
+            onClick={() => window.open(linkMore.href, '_self')}
+          >
+            {linkMore.title || intl.formatMessage(messages.view_all)}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
@@ -60,6 +74,7 @@ const ArgumentsTemplate = ({ items, isEditMode }) => {
 ArgumentsTemplate.propTypes = {
   items: PropTypes.arrayOf(PropTypes.any).isRequired,
   isEditMode: PropTypes.bool,
+  linkMore: PropTypes.any,
 };
 
 export default ArgumentsTemplate;
