f3007496076238893180602d46b4793473ee60c5
DamiDev <dami.developer@gmail.com>
2020-08-06 17:28:37 +0200

    added translation

--- src/components/ItaliaTheme/Blocks/ArgumentsInEvidence/BodyWrapper.jsx
+++ src/components/ItaliaTheme/Blocks/ArgumentsInEvidence/BodyWrapper.jsx
@@ -32,6 +32,10 @@ const messages = defineMessages({
     id: 'Vedi tutto',
     defaultMessage: 'Vedi tutti',
   },
+  otherArguments: {
+    id: 'otherArguments',
+    defaultMessage: 'ALTRI ARGOMENTI',
+  }
 })
 /**
  * Edit text block class.
@@ -85,7 +89,7 @@ class BodyWrapper extends SubblocksEdit {
         <div className="col-lg-10 col-xl-8 offset-lg-1 offset-xl-3 pt-5">
           <div className="row d-lg-inline-flex w-100">
             <div className="col-lg-3">
-              <h6 className="text-uppercase text-center mt-1">ALTRI ARGOMENTI</h6>
+              <h6 className="text-uppercase text-center mt-1">{this.props.intl?.formatMessage(messages.otherArguments)}</h6>
             </div>
             <div className="col-lg-9">
               {
