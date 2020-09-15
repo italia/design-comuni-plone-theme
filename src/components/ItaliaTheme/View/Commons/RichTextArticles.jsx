933a8fd786b7eca04fbff5c9a655eec1eb875a17
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-09-15 09:26:33 +0200

    updated venue view

--- src/components/ItaliaTheme/View/Commons/RichTextArticles.jsx
+++ src/components/ItaliaTheme/View/Commons/RichTextArticles.jsx
@@ -12,26 +12,27 @@ const RichTextArticles = ({ title, contents, tag_id, add_class, children }) => {
   return (
     <article id={tag_id} className="it-page-section anchor-offset mt-5">
       {title && <h4 id={`header-${tag_id}`}>{title}</h4>}
-      {contents?.length > 0 && 
-        contents.map((content, index) => (
-          content?.text && 
-            <div className="mt-3" key={index}>
-              {content.title && <h6>{content.title}</h6>}
-              {
-                content.href ? 
-                  <a href={content.href}
+      {contents?.length > 0 &&
+        contents.map(
+          (content, index) =>
+            content?.text && (
+              <div className="mt-3" key={index}>
+                {content.title && <h6>{content.title}:</h6>}
+                {content.href ? (
+                  <a
+                    href={content.href}
                     className={cx('text-serif', add_class)}
-                    dangerouslySetInnerHTML={{ __html: content.text }} 
+                    dangerouslySetInnerHTML={{ __html: content.text }}
                   />
-                :
+                ) : (
                   <div
                     className={cx('text-serif', add_class)}
                     dangerouslySetInnerHTML={{ __html: content.text }}
                   />
-              }
-            </div>
-        )
-      )}
+                )}
+              </div>
+            ),
+        )}
       {children}
     </article>
   );
