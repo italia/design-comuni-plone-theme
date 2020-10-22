ec8099911cbbda6553bd9330bd7b1c3b7a0771e0
nzambello <nzambello@protonmail.com>
2020-10-22 15:42:04 +0200

    dev: added styles for logo img

--- src/components/ItaliaTheme/Logo/Logo.jsx
+++ src/components/ItaliaTheme/Logo/Logo.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Icon } from 'design-react-kit/dist/design-react-kit';
 
+/*
+ * Customization with image
+ *
+ * If you have a jpg/png, do like this:
+ *
+ * <figure className="icon">
+ *  <img src={logo} alt="" />
+ * </figure>
+ *
+ * Note the icon class.
+ */
+
 const Logo = () => <Icon color="" icon="it-pa" padding={false} size="" />;
 
 export default Logo;
