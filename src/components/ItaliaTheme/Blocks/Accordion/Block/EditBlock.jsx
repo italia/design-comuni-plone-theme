d707c037a4e2ab1deee6f418fe5e31ed47ba0b6d
nzambello <nzambello@protonmail.com>
2020-11-12 12:02:12 +0100

    dev: added accordion block

---
+++ src/components/ItaliaTheme/Blocks/Accordion/Block/EditBlock.jsx
@@ -0,0 +1,154 @@
+/**
+ * Edit text block.
+ * @module components/manage/Blocks/Title/Edit
+ */
+
+import React from 'react';
+import { compose } from 'redux';
+import { injectIntl, defineMessages } from 'react-intl';
+import isSoftNewlineEvent from 'draft-js/lib/isSoftNewlineEvent';
+import {
+  DNDSubblocks,
+  SubblockEdit,
+  Subblock,
+} from '@italia/addons/volto-subblocks';
+import { Placeholder } from 'semantic-ui-react';
+import { Button, Icon } from 'design-react-kit/dist/design-react-kit';
+import { Editor, DefaultDraftBlockRenderMap, EditorState } from 'draft-js';
+
+import { TextEditorWidget } from '@italia/components/ItaliaTheme';
+
+const messages = defineMessages({
+  titlePlaceholder: {
+    id: 'Title placeholder',
+    defaultMessage: 'Title...',
+  },
+  textPlaceholder: {
+    id: 'Text placeholder',
+    defaultMessage: 'Text...',
+  },
+  vedi: {
+    id: 'Vedi',
+    defaultMessage: 'Vedi',
+  },
+});
+/**
+ * Edit text block class.
+ * @class Edit
+ * @extends Component
+ */
+class EditBlock extends SubblockEdit {
+  /**
+   * Constructor
+   * @method constructor
+   * @param {Object} props Component properties
+   * @constructs WysiwygEditor
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      focusOn: 'title',
+    };
+    this.accordion_item_ref = React.createRef();
+  }
+
+  componentDidMount() {
+    this.accordion_item_ref?.current?.addEventListener('keydown', (e) => {
+      if (e.keyCode == 13) {
+        e.stopPropagation();
+        if (!(this.state.focusOn === 'text')) {
+          this.setState({ focusOn: 'text' });
+        }
+      }
+    });
+  }
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    if (__SERVER__) {
+      return <div />;
+    }
+
+    return (
+      <Subblock subblock={this} className="subblock-edit">
+        <div ref={this.accordion_item_ref}>
+          <h3 className="accordion-header">
+            <Button
+              color="link"
+              tag="button"
+              onClick={(e) => {
+                if (this.props.selected) {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  this.props.onSubblockChangeFocus(-1);
+                }
+              }}
+            >
+              <Icon
+                color="primary"
+                icon={this.props.selected ? 'it-minus' : 'it-plus'}
+                padding={false}
+              />
+            </Button>
+
+            <div
+              onClick={() => {
+                this.setState({ focusOn: 'title' });
+              }}
+            >
+              <TextEditorWidget
+                data={this.props.data}
+                fieldName="title"
+                selected={this.props.selected && this.state.focusOn === 'title'}
+                block={this.props.block}
+                onChangeBlock={this.onChange}
+                placeholder={this.props.intl.formatMessage(
+                  messages.titlePlaceholder,
+                )}
+                nextFocus="text"
+                showToolbar={false}
+              />
+            </div>
+          </h3>
+          {this.props.selected && (
+            <div
+              className="accordion-content open"
+              onClick={() => {
+                this.setState({ focusOn: 'text' });
+              }}
+            >
+              <div className="accordion-inner">
+                <TextEditorWidget
+                  data={this.props.data}
+                  fieldName="text"
+                  selected={
+                    this.props.selected && this.state.focusOn === 'text'
+                  }
+                  block={this.props.block}
+                  onChangeBlock={this.onChange}
+                  placeholder={this.props.intl.formatMessage(
+                    messages.textPlaceholder,
+                  )}
+                />
+              </div>
+              {this.props.data.href && (
+                <div className="link-more">
+                  <a href={this.props.data.href}>
+                    {this.props.data.linkMoreTitle ||
+                      intl.formatMessage(messages.vedi)}
+                    <Icon icon="it-arrow-right" />
+                  </a>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </Subblock>
+    );
+  }
+}
+
+export default React.memo(compose(injectIntl, ...DNDSubblocks)(EditBlock));
