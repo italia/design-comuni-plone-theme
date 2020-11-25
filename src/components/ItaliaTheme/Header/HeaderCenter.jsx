7a507a222f202a8cd4ca5454daf6a574d3358f99
nzambello <nzambello@protonmail.com>
2020-11-25 16:21:19 +0100

    dev: mobile menu styles and content

--- src/components/ItaliaTheme/Header/HeaderCenter.jsx
+++ src/components/ItaliaTheme/Header/HeaderCenter.jsx
@@ -34,7 +34,7 @@ const HeaderCenter = () => {
   const intl = useIntl();
   const [showSearchModal, setShowSearchModal] = useState(false);
 
-  const subsite = useSelector(state => state.subsite?.data);
+  const subsite = useSelector((state) => state.subsite?.data);
 
   return (
     <Header small={false} theme="" type="center">
