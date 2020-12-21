708f2174e465c5726ef9ef15a74bb339d357c7e5
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-12-21 18:22:46 +0100

    removed 0 message if there's no results in site search on mobile

--- src/customizations/components/theme/Search/Search.jsx
+++ src/customizations/components/theme/Search/Search.jsx
@@ -184,8 +184,8 @@ const Search = () => {
     },
   ];
 
-  const getSectionFromId = id => {
-    let itemSection = Object.keys(sections).filter(s => id.indexOf(s) > -1);
+  const getSectionFromId = (id) => {
+    let itemSection = Object.keys(sections).filter((s) => id.indexOf(s) > -1);
 
     if (itemSection?.length > 0) {
       let sectionURL = `/${itemSection[0]}`;
@@ -209,7 +209,7 @@ const Search = () => {
     setCurrentPage(activePage?.children ?? 1);
   };
 
-  const searchFilters = useSelector(state => state.searchFilters.result);
+  const searchFilters = useSelector((state) => state.searchFilters.result);
   useEffect(() => {
     if (!searchFilters || Object.keys(searchFilters).length === 0)
       dispatch(getSearchFilters());
@@ -227,7 +227,7 @@ const Search = () => {
     setOptions(parseFetchedOptions({}, location));
   }, [searchFilters]);
 
-  const searchResults = useSelector(state => state.searchResults);
+  const searchResults = useSelector((state) => state.searchResults);
   useDebouncedEffect(
     () => {
       doSearch();
@@ -304,7 +304,7 @@ const Search = () => {
             <div className="d-block d-lg-none d-xl-none">
               <div className="row pb-3">
                 <div className="col-6">
-                  {searchResults?.result?.items_total && (
+                  {searchResults?.result?.items_total > 0 && (
                     <small>
                       {intl.formatMessage(messages.foundNResults, {
                         total: searchResults.result.items_total,
@@ -315,7 +315,7 @@ const Search = () => {
                 <div className="col-6">
                   <div className="float-right">
                     <a
-                      onClick={() => setCollapseFilters(prev => !prev)}
+                      onClick={() => setCollapseFilters((prev) => !prev)}
                       href="#categoryCollapse"
                       role="button"
                       className="font-weight-bold text-uppercase"
@@ -364,8 +364,9 @@ const Search = () => {
                 </div>
               </div>
 
-              {Object.values(options).filter(o => o !== null && o !== undefined)
-                .length > 0 && (
+              {Object.values(options).filter(
+                (o) => o !== null && o !== undefined,
+              ).length > 0 && (
                 <div className="pt-2 pt-lg-5">
                   <h6 className="text-uppercase">
                     {intl.formatMessage(messages.options)}
@@ -378,9 +379,9 @@ const Search = () => {
                         )}
                         id="options-active-content"
                         checked={options.activeContent}
-                        onChange={e => {
+                        onChange={(e) => {
                           const checked = e.currentTarget?.checked ?? false;
-                          setOptions(opts => ({
+                          setOptions((opts) => ({
                             ...opts,
                             activeContent: checked,
                           }));
@@ -400,7 +401,7 @@ const Search = () => {
                     role="presentation"
                     className="chip chip-lg selected"
                     onClick={() =>
-                      setOptions(opts => ({ ...opts, dateStart: null }))
+                      setOptions((opts) => ({ ...opts, dateStart: null }))
                     }
                   >
                     <span className="chip-label">
@@ -423,7 +424,7 @@ const Search = () => {
                     role="presentation"
                     className="chip chip-lg selected"
                     onClick={() =>
-                      setOptions(opts => ({ ...opts, dateEnd: null }))
+                      setOptions((opts) => ({ ...opts, dateEnd: null }))
                     }
                   >
                     <span className="chip-label">
@@ -462,17 +463,19 @@ const Search = () => {
                     <Col xs={6}>
                       <SelectInput
                         id="search-sort-on"
-                        value={sortOnOptions.filter(o => o.value === sortOn)[0]}
+                        value={
+                          sortOnOptions.filter((o) => o.value === sortOn)[0]
+                        }
                         label={intl.formatMessage(messages.orderBy)}
                         placeholder={intl.formatMessage(messages.orderBy)}
-                        onChange={opt => setSortOn(opt.value)}
+                        onChange={(opt) => setSortOn(opt.value)}
                         options={sortOnOptions}
                       />
                     </Col>
                   </Row>
                 </div>
                 <Row>
-                  {searchResults?.result?.items?.map(i => (
+                  {searchResults?.result?.items?.map((i) => (
                     <Col md={4} key={i['@id']}>
                       <Card
                         teaser
