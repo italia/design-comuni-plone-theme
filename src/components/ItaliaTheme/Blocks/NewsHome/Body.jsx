56d25897a4618efaae10ad69d8db4efedb745c83
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-12-22 16:53:51 +0100

    fixed import for NewsHomeblock

--- src/components/ItaliaTheme/Blocks/NewsHome/Body.jsx
+++ src/components/ItaliaTheme/Blocks/NewsHome/Body.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
+import { Link } from 'react-router-dom';
 import { UniversalLink } from '@plone/volto/components';
 import moment from 'moment';
 import {
