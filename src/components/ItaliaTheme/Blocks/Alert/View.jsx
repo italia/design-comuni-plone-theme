dad8ea33de1e84ebf21cc0af5956facb06b8a1a8
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-01-07 11:14:21 +0100

    added role alert to Alert View Block2

--- src/components/ItaliaTheme/Blocks/Alert/View.jsx
+++ src/components/ItaliaTheme/Blocks/Alert/View.jsx
@@ -8,9 +8,9 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import redraft from 'redraft';
 import { Container, Row, Col } from 'design-react-kit/dist/design-react-kit';
-import { isCmsUi } from '@plone/volto/helpers';
+//import { isCmsUi } from '@plone/volto/helpers';
 import { settings } from '~/config';
-import { flattenToAppURL } from '@plone/volto/helpers';
+
 /**
  * View Alert block class.
  * @class View
@@ -20,18 +20,14 @@ const View = ({ data, pathname }) => {
   if (__SERVER__) {
     return <div />;
   }
-  const isCmsUI = pathname ? isCmsUi(pathname) : false;
-  const generateKey = (pre) => {
-    pre = pre ? pre : '';
-    return pre + Math.random(5);
-  };
+  //const isCmsUI = pathname ? isCmsUi(pathname) : false;
 
   const content = data.text
     ? redraft(data.text, settings.ToHTMLRenderers, settings.ToHTMLOptions)
     : '';
 
   return (
-    <section className="block alertblock" key={generateKey('alert')}>
+    <section role="alert" className="block alertblock">
       <Row className={cx('row-full-width', 'bg-alert-' + data.color)}>
         <Container className="p-4 pt-5 pb-5">
           <Row className="align-items-start">
