57c37993e4389a4d888ec3022e5285b6cd5b512b
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-01-08 17:44:19 +0100

    added field type Attachment in Form block

--- src/components/ItaliaTheme/Blocks/Form/Field.jsx
+++ src/components/ItaliaTheme/Blocks/Form/Field.jsx
@@ -12,6 +12,7 @@ import {
   Label,
 } from 'design-react-kit/dist/design-react-kit';
 import Select from 'react-select';
+import FileWidget from '@italia/components/ItaliaTheme/Blocks/Form/Widgets/FileWidget.jsx';
 
 const messages = defineMessages({
   select_a_value: {
@@ -196,6 +197,20 @@ const Field = ({
           onChange={(e) => {
             onChange(name, e.target.value);
           }}
+        />
+      )}
+      {field_type === 'attachment' && (
+        <FileWidget
+          id={name}
+          name={name}
+          label={getLabel()}
+          type="file"
+          required={required}
+          infoText={description}
+          invalid={isInvalid()}
+          onChange={onChange}
+          onEdit={isOnEdit}
+          value={value}
         />
       )}
       {field_type === 'from' && (
