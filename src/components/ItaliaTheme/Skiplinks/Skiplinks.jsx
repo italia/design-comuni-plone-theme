be3c8dd54e6a268c4a62861ac4bdefd107f76abd
Nicola Zambello <nzambello@protonmail.com>
2021-02-23 15:31:23 +0100

    refactor: removed useless fragment

--- src/components/ItaliaTheme/Skiplinks/Skiplinks.jsx
+++ src/components/ItaliaTheme/Skiplinks/Skiplinks.jsx
@@ -21,19 +21,17 @@ const Skiplinks = () => {
   const intl = useIntl();
 
   return (
-    <>
-      <Skiplink tag="div">
-        <SkiplinkItem href="#view" tag="a">
-          {intl.formatMessage(messages.mainView)}
-        </SkiplinkItem>
-        <SkiplinkItem href="#navigation" tag="a">
-          {intl.formatMessage(messages.navigation)}
-        </SkiplinkItem>
-        <SkiplinkItem href="#footer" tag="a">
-          {intl.formatMessage(messages.footer)}
-        </SkiplinkItem>
-      </Skiplink>
-    </>
+    <Skiplink tag="div">
+      <SkiplinkItem href="#view" tag="a">
+        {intl.formatMessage(messages.mainView)}
+      </SkiplinkItem>
+      <SkiplinkItem href="#navigation" tag="a">
+        {intl.formatMessage(messages.navigation)}
+      </SkiplinkItem>
+      <SkiplinkItem href="#footer" tag="a">
+        {intl.formatMessage(messages.footer)}
+      </SkiplinkItem>
+    </Skiplink>
   );
 };
 
