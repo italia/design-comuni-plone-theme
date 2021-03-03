db34c7091937976599d2a08ec5df13a53741bd1e
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-03-03 10:01:49 +0100

    feat: added arLoginUrl to siteConfig, to show login button in HeaderSlim

--- src/components/ItaliaTheme/Header/HeaderSlim/HeaderSlimRightZone.jsx
+++ src/components/ItaliaTheme/Header/HeaderSlim/HeaderSlimRightZone.jsx
@@ -5,42 +5,50 @@
 
 import React from 'react';
 
-//import { Link } from 'react-router-dom';
-//import { defineMessages, useIntl } from 'react-intl';
+import { Link } from 'react-router-dom';
+import { defineMessages, useIntl } from 'react-intl';
+import { Button } from 'design-react-kit/dist/design-react-kit';
+
+import { siteConfig } from '~/config';
 
 import {
   ParentSiteMenu,
   LanguageSelector,
+  Icon,
 } from '@italia/components/ItaliaTheme';
 
-// const messages = defineMessages({
-//   arLogin: {
-//     id: "Accedi all'area personale",
-//     defaultMessage: "Accedi all'area personale",
-//   },
-// });
+const messages = defineMessages({
+  arLogin: {
+    id: "Accedi all'area personale",
+    defaultMessage: "Accedi all'area personale",
+  },
+});
 
 const HeaderSlimRightZone = () => {
+  const intl = useIntl();
   return (
     <>
       <ParentSiteMenu />
       <LanguageSelector />
 
-      {/*<Button
-            className="btn-icon"
-            color="primary"
-            to="#"
-            icon={false}
-            size="full"
-            tag={Link}
-          >
-            <span className="rounded-icon">
-              <Icon color="primary" icon="it-user" padding={false} size="" />
-            </span>
-            <span className="d-none d-lg-block">
-              {intl.formatMessage(messages.arLogin)}
-            </span>
-          </Button>*/}
+      {siteConfig.properties.arLoginUrl && (
+        <Button
+          className="btn-icon"
+          color="primary"
+          to={siteConfig.properties.arLoginUrl}
+          target="_blank"
+          icon={false}
+          size="full"
+          tag={Link}
+        >
+          <span className="rounded-icon">
+            <Icon color="primary" icon="it-user" padding={false} size="" />
+          </span>
+          <span className="d-none d-lg-block">
+            {intl.formatMessage(messages.arLogin)}
+          </span>
+        </Button>
+      )}
     </>
   );
 };
--- src/config.js
+++ src/config.js
@@ -553,6 +553,7 @@ export const siteConfig = {
     footerInfos:
       'Via Roma 0 - 00000 Lorem Ipsum Codice fiscale / P. IVA: 000000000',
     amministrazioneTrasparenteUrl: '/amministrazione-trasparente',
+    //arLoginUrl: '/area-riservata',
   },
 };
 
