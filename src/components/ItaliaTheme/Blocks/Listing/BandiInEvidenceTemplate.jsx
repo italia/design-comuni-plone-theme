f0de554947e3ad533efe74cbf0ae49d87a199a40
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-04-08 11:24:40 +0200

    fix: bandiInEvidenceTemplate

--- src/components/ItaliaTheme/Blocks/Listing/BandiInEvidenceTemplate.jsx
+++ src/components/ItaliaTheme/Blocks/Listing/BandiInEvidenceTemplate.jsx
@@ -24,11 +24,15 @@ const messages = defineMessages({
   },
   pubblicazione: {
     id: 'bando_data_pubblicazione',
-    defaultMessage: 'Data di pubblicazione:',
+    defaultMessage: 'Pubblicato il',
   },
   scadenza: {
     id: 'bando_scadenza',
-    defaultMessage: 'Data di scadenza:',
+    defaultMessage: 'Scadenza partecipazione',
+  },
+  chiusura_procedimento: {
+    id: 'chiusura_procedimento_bando',
+    defaultMessage: 'Chiusura del procedimento',
   },
   stato: {
     id: 'bando_stato',
@@ -63,104 +67,115 @@ const BandiInEvidenceTemplate = ({
         'public-ui': isEditMode,
       })}
     >
-      <div className="full-width">
-        <Container className="listing-container">
-          {title && (
-            <Row>
-              <Col>
-                <h2 className={cx('mb-4', { 'mt-5': !show_block_bg })}>
-                  {title}
-                </h2>
-              </Col>
-            </Row>
-          )}
-          <div className="bandi-in-evidence-cards-wrapper mb-5">
-            {items.map((item, index) => {
-              const listingText = <ListingText item={item} />;
-              return (
-                <Card key={index} className={cx('listing-item card-bg', '')}>
-                  <CardBody>
-                    <CardTitle tag="h3" className="title">
-                      <UniversalLink
-                        className="bando-title"
-                        item={!isEditMode ? item : null}
-                        href={isEditMode ? '#' : null}
-                      >
-                        {item.title || item.id}
-                      </UniversalLink>
-                    </CardTitle>
-                    <div className="bando-description">{item.description}</div>
-                    <div className={'bando-dati'}>
-                      <span className=" d-flex align-items-baseline bando-dati-info">
-                        <div className="bando-dati-label mr-4">
-                          {intl.formatMessage(messages.pubblicazione)}
-                        </div>
-                        <span className="bando-dati-date">
-                          {item.effective &&
-                            moment(item.effective).format('DD-MM-YYYY')}
-                        </span>
+      <Container className="px-4">
+        {title && (
+          <Row>
+            <Col>
+              <h2 className={cx('mb-4', { 'mt-5': !show_block_bg })}>
+                {title}
+              </h2>
+            </Col>
+          </Row>
+        )}
+        <div className="bandi-in-evidence-cards-wrapper mb-5">
+          {items.map((item, index) => {
+            const listingText = <ListingText item={item} />;
+            return (
+              <Card key={index} className={cx('listing-item card-bg', '')}>
+                <CardBody>
+                  <CardTitle tag="h3" className="title">
+                    <UniversalLink
+                      className="bando-title"
+                      item={!isEditMode ? item : null}
+                      href={isEditMode ? '#' : null}
+                    >
+                      {item.title || item.id}
+                    </UniversalLink>
+                  </CardTitle>
+
+                  {listingText && (
+                    <div className="bando-description">{listingText}</div>
+                  )}
+                  <div className="bando-dati mb-2">
+                    <span className="d-flex flex-wrap align-items-baseline bando-dati-info">
+                      <div className="bando-dati-label mr-2">
+                        {intl.formatMessage(messages.pubblicazione)}:
+                      </div>
+                      <span className="bando-dati-date">
+                        {item.effective &&
+                          moment(item.effective).format('DD-MM-YYYY')}
                       </span>
-                      <span className="d-flex align-items-baseline bando-dati-info">
-                        <div className="bando-dati-label mr-4">
-                          {intl.formatMessage(messages.scadenza)}
+                    </span>
+                    {item.scadenza_bando && (
+                      <span className="d-flex flex-wrap align-items-baseline bando-dati-info">
+                        <div className="bando-dati-label mr-2">
+                          {intl.formatMessage(messages.scadenza)}:
                         </div>
                         <span className="bando-dati-date">
                           {item.scadenza_bando &&
                             moment(item.scadenza_bando).format('DD-MM-YYYY')}
                         </span>
                       </span>
-                      <span className="d-flex align-items-baseline bando-dati-info">
-                        <div className="bando-dati-label mr-4">
-                          {intl.formatMessage(messages.stato)}
+                    )}
+                    {item.chiusura_procedimento_bando && (
+                      <span className="d-flex flex-wrap align-items-baseline bando-dati-info">
+                        <div className="bando-dati-label mr-2">
+                          {intl.formatMessage(messages.chiusura_procedimento)}:
                         </div>
-                        {item?.bando_state && (
-                          <span className="bando-dati-date">
-                            <div
-                              className={cx('bando-state', {
-                                open: item.bando_state?.includes('open'),
-                                closed: item.bando_state?.includes('closed'),
-                                'in-progress': item.bando_state?.includes(
-                                  'inProgress',
-                                ),
-                              })}
-                            >
-                              {intl.formatMessage(
-                                messages[item.bando_state[0]],
-                              )}
-                            </div>
-                          </span>
-                        )}
+                        <span className="bando-dati-date">
+                          {item.chiusura_procedimento_bando &&
+                            moment(item.chiusura_procedimento_bando).format(
+                              'DD-MM-YYYY',
+                            )}
+                        </span>
                       </span>
-                      {listingText && (
-                        <span className="bando-dati-info">{listingText}</span>
+                    )}
+                    <span className="d-flex align-items-baseline bando-dati-info">
+                      <div className="bando-dati-label mr-3">
+                        {intl.formatMessage(messages.stato)}:
+                      </div>
+                      {item?.bando_state && (
+                        <span className="bando-dati-date">
+                          <div
+                            className={cx('bando-state', {
+                              open: item.bando_state?.includes('open'),
+                              closed: item.bando_state?.includes('closed'),
+                              'in-progress': item.bando_state?.includes(
+                                'inProgress',
+                              ),
+                            })}
+                          >
+                            {intl.formatMessage(messages[item.bando_state[0]])}
+                          </div>
+                        </span>
                       )}
-                    </div>
-                    <div className="read-more">
-                      <CardReadMore
-                        iconName="it-arrow-right"
-                        tag={UniversalLink}
-                        item={!isEditMode ? item : null}
-                        href={isEditMode ? '#' : null}
-                        text={intl.formatMessage(messages.vedi)}
-                      />
-                    </div>
-                  </CardBody>
-                </Card>
-              );
-            })}
+                    </span>
+                  </div>
+                  <div className="read-more">
+                    <CardReadMore
+                      iconName="it-arrow-right"
+                      tag={UniversalLink}
+                      item={!isEditMode ? item : null}
+                      href={isEditMode ? '#' : null}
+                      text={intl.formatMessage(messages.vedi)}
+                    />
+                  </div>
+                </CardBody>
+              </Card>
+            );
+          })}
+        </div>
+        {linkMore?.href && (
+          <div className="link-button text-center my-4">
+            <UniversalLink
+              href={flattenToAppURL(linkMore.href)}
+              className="btn btn-tertiary"
+            >
+              {linkMore.title || intl.formatMessage(messages.view_all)}
+            </UniversalLink>
           </div>
-          {linkMore?.href && (
-            <div className="link-button text-center my-4">
-              <UniversalLink
-                href={flattenToAppURL(linkMore.href)}
-                className="btn btn-tertiary"
-              >
-                {linkMore.title || intl.formatMessage(messages.view_all)}
-              </UniversalLink>
-            </div>
-          )}
-        </Container>
-      </div>
+        )}
+      </Container>
     </div>
   );
 };
