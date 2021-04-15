a42473060aeec8771967b038614c9933e0e8c197
Nicola Zambello <nzambello@protonmail.com>
2021-04-15 10:57:43 +0200

    fix: listing body selection state

--- src/customizations/volto/components/manage/Blocks/Listing/ListingBody.jsx
+++ src/customizations/volto/components/manage/Blocks/Listing/ListingBody.jsx
@@ -5,248 +5,255 @@ import { FormattedMessage, injectIntl } from 'react-intl';
 import { getQueryStringResults, getContent } from '@plone/volto/actions';
 import { Pagination, Skeleton } from '@italia/components/ItaliaTheme';
 import { setOriginalQuery } from '@italia/actions';
+import { isEqual } from 'lodash';
 
 import config from '@plone/volto/registry';
 
-const ListingBody = ({ data, properties, intl, path, isEditMode }) => {
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const content = useSelector((state) => state.content.data);
-  const querystringResults = useSelector(
-    (state) => state.querystringsearch.subrequests,
-  );
-  const [firstLoading, setFirstLoading] = React.useState(true);
-  const dispatch = useDispatch();
-  const listingRef = createRef();
-  const [additionalFilters, setAdditionalFilters] = React.useState([]);
-  const originalQuery = useSelector((state) => {
-    return state.originalQuery?.[properties['@id']]?.[data.block]?.toArray();
-  });
+const ListingBody = React.memo(
+  ({ data, properties, intl, path, isEditMode }) => {
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const content = useSelector((state) => state.content.data);
+    const querystringResults = useSelector(
+      (state) => state.querystringsearch.subrequests?.[data.block],
+    );
+    const [firstLoading, setFirstLoading] = React.useState(true);
+    const dispatch = useDispatch();
+    const listingRef = createRef();
+    const [additionalFilters, setAdditionalFilters] = React.useState([]);
+    const originalQuery = useSelector((state) => {
+      return state.originalQuery?.[properties['@id']]?.[
+        data.block
+      ]?.toArray?.();
+    });
 
-  useEffect(() => {
-    if (
-      !originalQuery &&
-      properties['@id'] &&
-      data.block &&
-      data.query?.length > 0
-    ) {
-      dispatch(
-        setOriginalQuery(
-          properties['@id'],
-          data.block,
-          JSON.parse(JSON.stringify(data.query)),
-        ),
-      );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    useEffect(() => {
+      if (
+        !originalQuery &&
+        properties['@id'] &&
+        data.block &&
+        data.query?.length > 0
+      ) {
+        dispatch(
+          setOriginalQuery(
+            properties['@id'],
+            data.block,
+            JSON.parse(JSON.stringify(data.query)),
+          ),
+        );
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  useEffect(() => {
-    if (
-      data?.query?.length > 0 &&
-      !querystringResults?.[data.block]?.loading &&
-      (isEditMode || (!isEditMode && !querystringResults?.[data.block]?.loaded))
-    ) {
-      doSearch(data);
-    }
-    /* eslint-disable react-hooks/exhaustive-deps */
-  }, [data]);
+    useEffect(() => {
+      if (
+        data?.query?.length > 0 &&
+        !querystringResults?.loading &&
+        (isEditMode || (!isEditMode && !querystringResults?.loaded))
+      ) {
+        doSearch(data);
+      }
+      /* eslint-disable react-hooks/exhaustive-deps */
+    }, [data]);
 
-  const doSearch = (data = { query: [] }, page = 1) => {
-    if (data?.query?.length > 0 || additionalFilters.length > 0) {
-      let query = [
-        ...(originalQuery
-          ? JSON.parse(JSON.stringify(originalQuery))
-          : data.query),
-      ];
+    const doSearch = (data = { query: [] }, page = 1) => {
+      if (data.query?.length > 0 || additionalFilters.length > 0) {
+        let query = [
+          ...(originalQuery && additionalFilters.length > 0
+            ? JSON.parse(JSON.stringify(originalQuery))
+            : data.query),
+        ];
 
-      //faccio l'override dei filtri di default
-      additionalFilters.forEach((filter) => {
-        let replaced = false;
-        query.forEach((f) => {
-          if (f.i === filter.i && f.o === filter.o) {
-            replaced = true;
-            f.v = filter.v;
+        //faccio l'override dei filtri di default
+        additionalFilters.forEach((filter) => {
+          let replaced = false;
+          query.forEach((f) => {
+            if (f.i === filter.i && f.o === filter.o) {
+              replaced = true;
+              f.v = filter.v;
+            }
+          });
+          if (!replaced) {
+            query.push(filter);
           }
         });
-        if (!replaced) {
-          query.push(filter);
-        }
-      });
 
-      let _data = { ...data, query: query };
+        let _data = { ...data, query: query };
 
-      dispatch(
-        getQueryStringResults(
-          path,
-          { ..._data, fullobjects: 1 },
-          data.block,
-          page,
-        ),
-      );
-    } else if (data.template === 'imageGallery' && data?.query?.length === 0) {
-      dispatch(
-        getQueryStringResults(
-          path,
-          {
-            ...data,
-            fullobjects: 1,
-            query: [
-              {
-                i: 'path',
-                o: 'plone.app.querystring.operation.string.relativePath',
-                v: '',
-              },
-            ],
-          },
-          data.block,
-          page,
-        ),
-      );
+        dispatch(
+          getQueryStringResults(
+            path,
+            { ..._data, fullobjects: 1 },
+            data.block,
+            page,
+          ),
+        );
+      } else if (
+        data.template === 'imageGallery' &&
+        data?.query?.length === 0
+      ) {
+        dispatch(
+          getQueryStringResults(
+            path,
+            {
+              ...data,
+              fullobjects: 1,
+              query: [
+                {
+                  i: 'path',
+                  o: 'plone.app.querystring.operation.string.relativePath',
+                  v: '',
+                },
+              ],
+            },
+            data.block,
+            page,
+          ),
+        );
+      }
+    };
+
+    useEffect(() => {
+      if (!firstLoading) {
+        doSearch(data);
+      }
+    }, [additionalFilters]);
+
+    const addFilters = (filters = []) => {
+      setCurrentPage(1);
+      setAdditionalFilters(filters);
+    };
+
+    const folderItems = content?.is_folderish ? content.items : [];
+
+    const loadingQuery = data?.query?.length > 0 && querystringResults?.loading;
+
+    if (firstLoading && querystringResults && !loadingQuery) {
+      setFirstLoading(false);
     }
-  };
+    const listingItems =
+      data?.query?.length > 0 ? querystringResults?.items || [] : folderItems;
 
-  useEffect(() => {
-    if (!firstLoading) {
-      doSearch(data);
+    const templateConfig = config.blocks.blocksConfig.listing.templates;
+
+    let templateName =
+      data.template && !!templateConfig[data.template]
+        ? data.template
+        : 'default';
+
+    const ListingBodyTemplate = templateConfig[templateName].template;
+    const SkeletonTemplate = templateConfig[templateName].skeleton || Skeleton;
+
+    function handleContentPaginationChange(e, { activePage }) {
+      !isEditMode && listingRef.current.scrollIntoView({ behavior: 'smooth' });
+      const current = activePage?.children ?? 1;
+      setCurrentPage(current);
+      dispatch(getContent(path, null, null, current));
     }
-  }, [additionalFilters]);
 
-  const addFilters = (filters = []) => {
-    setCurrentPage(1);
-    setAdditionalFilters(filters);
-  };
-
-  const folderItems = content?.is_folderish ? content.items : [];
-
-  const loadingQuery =
-    data?.query?.length > 0 && querystringResults?.[data.block]?.loading;
-
-  if (firstLoading && querystringResults?.[data.block] && !loadingQuery) {
-    setFirstLoading(false);
-  }
-  const listingItems =
-    data?.query?.length > 0
-      ? (querystringResults &&
-          querystringResults[data.block] &&
-          querystringResults[data.block].items) ||
-        []
-      : folderItems;
-
-  const templateConfig = config.blocks.blocksConfig.listing.templates;
-
-  let templateName =
-    data.template && !!templateConfig[data.template]
-      ? data.template
-      : 'default';
-
-  const ListingBodyTemplate = templateConfig[templateName].template;
-  const SkeletonTemplate = templateConfig[templateName].skeleton || Skeleton;
-
-  function handleContentPaginationChange(e, { activePage }) {
-    !isEditMode && listingRef.current.scrollIntoView({ behavior: 'smooth' });
-    const current = activePage?.children ?? 1;
-    setCurrentPage(current);
-    dispatch(getContent(path, null, null, current));
-  }
-
-  function handleQueryPaginationChange(e, { activePage }) {
-    !isEditMode && listingRef.current.scrollIntoView({ behavior: 'smooth' });
-    const current = activePage?.children ?? 1;
-    setCurrentPage(current);
-    doSearch(data, current);
-  }
-
-  const getBackgroundClass = () => {
-    const block = properties.blocks[data.block];
-    if (!block?.show_block_bg) return '';
-
-    let bg_color = data.bg_color ? `bg-${data.bg_color}` : '';
-
-    if (block.template === 'gridGalleryTemplate') {
-      return `section section-muted section-inset-shadow py-5 ${bg_color}`;
-    } else {
-      return `bg-light py-5 ${bg_color}`;
+    function handleQueryPaginationChange(e, { activePage }) {
+      !isEditMode && listingRef.current.scrollIntoView({ behavior: 'smooth' });
+      const current = activePage?.children ?? 1;
+      setCurrentPage(current);
+      doSearch(data, current);
     }
-  };
 
-  const getBlockClasses = () => {
-    let bg_color = getBackgroundClass();
-    let items_color = data.items_color ? `items-color-${data.items_color}` : '';
+    const getBackgroundClass = () => {
+      const block = properties.blocks[data.block];
+      if (!block?.show_block_bg) return '';
 
-    return `${bg_color} ${items_color}`;
-  };
+      let bg_color = data.bg_color ? `bg-${data.bg_color}` : '';
 
-  return (
-    <div className="public-ui">
-      {loadingQuery && (
-        <div className={`full-width ${getBlockClasses()}`} ref={listingRef}>
-          <SkeletonTemplate {...data} />
-        </div>
-      )}
+      if (block.template === 'gridGalleryTemplate') {
+        return `section section-muted section-inset-shadow py-5 ${bg_color}`;
+      } else {
+        return `bg-light py-5 ${bg_color}`;
+      }
+    };
 
-      {!loadingQuery &&
-      (listingItems.length > 0 || additionalFilters?.length > 0) ? (
-        <div className={`full-width ${getBlockClasses()}`} ref={listingRef}>
-          <ListingBodyTemplate
-            items={listingItems}
-            isEditMode={isEditMode}
-            {...data}
-            addFilters={addFilters}
-            additionalFilters={additionalFilters}
-            items_total={
-              data?.query?.length === 0
-                ? content?.items_total
-                : querystringResults?.[data.block]?.total
-            }
-            loading={loadingQuery}
-            firstLoading={firstLoading}
-          />
-          {data?.query?.length === 0 &&
-            content?.items_total > config.settings.defaultPageSize && (
-              <div className="pagination-wrapper">
-                <Pagination
-                  activePage={currentPage}
-                  totalPages={Math.ceil(
-                    content.items_total / config.settings.defaultPageSize,
-                  )}
-                  onPageChange={handleContentPaginationChange}
-                />
-              </div>
-            )}
-          {(data?.query?.length > 0 || additionalFilters?.length > 0) &&
-            querystringResults[data.block].total >
-              (data.b_size || config.settings.defaultPageSize) && (
-              <div className="pagination-wrapper" key={currentPage}>
-                <Pagination
-                  activePage={currentPage}
-                  totalPages={Math.ceil(
-                    querystringResults[data.block].total /
-                      (data.b_size || config.settings.defaultPageSize),
-                  )}
-                  onPageChange={handleQueryPaginationChange}
-                />
-              </div>
-            )}
-        </div>
-      ) : isEditMode ? (
-        <div className="listing message">
-          {data?.query?.length === 0 && (
-            <FormattedMessage
-              id="No items found in this container."
-              defaultMessage="No items found in this container."
+    const getBlockClasses = () => {
+      let bg_color = getBackgroundClass();
+      let items_color = data.items_color
+        ? `items-color-${data.items_color}`
+        : '';
+
+      return `${bg_color} ${items_color}`;
+    };
+
+    return (
+      <div className="public-ui">
+        {loadingQuery && (
+          <div className={`full-width ${getBlockClasses()}`} ref={listingRef}>
+            <SkeletonTemplate {...data} />
+          </div>
+        )}
+
+        {!loadingQuery &&
+        (listingItems.length > 0 || additionalFilters?.length > 0) ? (
+          <div className={`full-width ${getBlockClasses()}`} ref={listingRef}>
+            <ListingBodyTemplate
+              items={listingItems}
+              isEditMode={isEditMode}
+              {...data}
+              addFilters={addFilters}
+              additionalFilters={additionalFilters}
+              items_total={
+                data?.query?.length === 0
+                  ? content?.items_total
+                  : querystringResults?.total
+              }
+              loading={loadingQuery}
+              firstLoading={firstLoading}
             />
-          )}
-          {!loadingQuery && data?.query?.length > 0 && (
-            <FormattedMessage
-              id="No results found."
-              defaultMessage="No results found."
-            />
-          )}
-        </div>
-      ) : null}
-    </div>
-  );
-};
+            {data?.query?.length === 0 &&
+              content?.items_total > config.settings.defaultPageSize && (
+                <div className="pagination-wrapper">
+                  <Pagination
+                    activePage={currentPage}
+                    totalPages={Math.ceil(
+                      content.items_total / config.settings.defaultPageSize,
+                    )}
+                    onPageChange={handleContentPaginationChange}
+                  />
+                </div>
+              )}
+            {(data?.query?.length > 0 || additionalFilters?.length > 0) &&
+              querystringResults.total >
+                (data.b_size || config.settings.defaultPageSize) && (
+                <div className="pagination-wrapper" key={currentPage}>
+                  <Pagination
+                    activePage={currentPage}
+                    totalPages={Math.ceil(
+                      querystringResults.total /
+                        (data.b_size || config.settings.defaultPageSize),
+                    )}
+                    onPageChange={handleQueryPaginationChange}
+                  />
+                </div>
+              )}
+          </div>
+        ) : isEditMode ? (
+          <div className="listing message">
+            {data?.query?.length === 0 && (
+              <FormattedMessage
+                id="No items found in this container."
+                defaultMessage="No items found in this container."
+              />
+            )}
+            {!loadingQuery && data?.query?.length > 0 && (
+              <FormattedMessage
+                id="No results found."
+                defaultMessage="No results found."
+              />
+            )}
+          </div>
+        ) : null}
+      </div>
+    );
+  },
+  function areEqual(prevProps, nextProps) {
+    return isEqual(prevProps.data, nextProps.data);
+  },
+);
 
 ListingBody.propTypes = {
   data: PropTypes.objectOf(PropTypes.any).isRequired,
