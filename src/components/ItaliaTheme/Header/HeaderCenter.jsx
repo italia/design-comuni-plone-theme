e020f5068b09138f706406ce3f443e1dd2c1d06b
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-05-13 14:19:29 +0200

    chore: moved header search to a dedicated component

--- src/components/ItaliaTheme/Header/HeaderCenter.jsx
+++ src/components/ItaliaTheme/Header/HeaderCenter.jsx
@@ -3,35 +3,25 @@
  * @module components/Header/HeaderCenter
  */
 
-import React, { useState } from 'react';
+import React from 'react';
 import { UniversalLink } from '@plone/volto/components';
 import { useSelector } from 'react-redux';
-import { defineMessages, useIntl } from 'react-intl';
+import { useIntl } from 'react-intl';
 import {
   Header,
   HeaderContent,
   HeaderRightZone,
 } from 'design-react-kit/dist/design-react-kit';
-import { Icon } from '@italia/components/ItaliaTheme';
-
 import { flattenToAppURL } from '@plone/volto/helpers';
 import {
-  SearchModal,
   Logo,
   SocialHeader,
+  HeaderSearch,
 } from '@italia/components/ItaliaTheme';
 import { getSiteProperty } from '@italia/helpers';
 
-const messages = defineMessages({
-  search: {
-    id: 'Cerca',
-    defaultMessage: 'Cerca',
-  },
-});
-
 const HeaderCenter = () => {
   const intl = useIntl();
-  const [showSearchModal, setShowSearchModal] = useState(false);
 
   const subsite = useSelector((state) => state.subsite?.data);
 
@@ -56,27 +46,7 @@ const HeaderCenter = () => {
         </div>
         <HeaderRightZone>
           <SocialHeader />
-
-          <div className="it-search-wrapper">
-            <span className="d-none d-md-block">
-              {intl.formatMessage(messages.search)}
-            </span>
-            <a
-              className="search-link rounded-icon"
-              aria-label={intl.formatMessage(messages.search)}
-              onClick={(e) => {
-                e.preventDefault();
-                setShowSearchModal(true);
-              }}
-              href="#search"
-            >
-              <Icon icon="it-search" />
-            </a>
-          </div>
-          <SearchModal
-            show={showSearchModal}
-            closeModal={() => setShowSearchModal(false)}
-          />
+          <HeaderSearch />
         </HeaderRightZone>
       </HeaderContent>
     </Header>
---
+++ src/components/ItaliaTheme/Header/HeaderSearch/HeaderSearch.jsx
@@ -0,0 +1,48 @@
+/**
+ * HeaderSearch component.
+ * @module components/Header/HeaderSearch
+ */
+
+import React, { useState } from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+import { Icon } from '@italia/components/ItaliaTheme';
+import { SearchModal } from '@italia/components/ItaliaTheme';
+
+const messages = defineMessages({
+  search: {
+    id: 'Cerca',
+    defaultMessage: 'Cerca',
+  },
+});
+
+const HeaderSearch = () => {
+  const intl = useIntl();
+  const [showSearchModal, setShowSearchModal] = useState(false);
+
+  return (
+    <>
+      <div className="it-search-wrapper">
+        <span className="d-none d-md-block">
+          {intl.formatMessage(messages.search)}
+        </span>
+        <a
+          className="search-link rounded-icon"
+          aria-label={intl.formatMessage(messages.search)}
+          onClick={(e) => {
+            e.preventDefault();
+            setShowSearchModal(true);
+          }}
+          href="#search"
+        >
+          <Icon icon="it-search" />
+        </a>
+      </div>
+      <SearchModal
+        show={showSearchModal}
+        closeModal={() => setShowSearchModal(false)}
+      />
+    </>
+  );
+};
+
+export default HeaderSearch;
