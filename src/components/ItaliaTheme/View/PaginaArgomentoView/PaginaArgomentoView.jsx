3e8acb5dfd111bd39fad3679678ea743e52111fa
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-06-23 08:27:25 +0200

    chore: viewed related items in Argomento

--- src/components/ItaliaTheme/View/PaginaArgomentoView/PaginaArgomentoView.jsx
+++ src/components/ItaliaTheme/View/PaginaArgomentoView/PaginaArgomentoView.jsx
@@ -26,6 +26,8 @@ import {
   TextOrBlocks,
   RichText,
   PaginaArgomentoViewNoBlocks,
+  RelatedItems,
+  RelatedItemInEvidence,
 } from '@italia/components/ItaliaTheme/View';
 import Image from '@plone/volto/components/theme/Image/Image';
 
@@ -58,78 +60,84 @@ const PaginaArgomentoView = ({ content }) => {
   }, [dispatch, content]);
 
   return hasBlocksData(content) ? (
-    <div id="page-document" className="ui container">
-      <div className="ArgomentoTitleWrapper mb-5">
-        <div className="title-description-wrapper col-lg-6">
-          <ArgumentIcon icon={content.icona} />
-          <h1 className="mb-3">{content?.title}</h1>
-          <p className="description">{content?.description}</p>
-        </div>
-        <div className="col-lg-4 offset-lg-2">
-          <RichText serif={false} content={content.ulteriori_informazioni} />
+    <div id="page-document">
+      <div className="ui container">
+        <div className="ArgomentoTitleWrapper mb-5">
+          <div className="title-description-wrapper col-lg-6">
+            <ArgumentIcon icon={content.icona} />
+            <h1 className="mb-3">{content?.title}</h1>
+            <p className="description">{content?.description}</p>
+          </div>
+          <div className="col-lg-4 offset-lg-2">
+            <RichText serif={false} content={content.ulteriori_informazioni} />
 
-          {content?.unita_amministrative_responsabili?.length > 0 &&
-            content?.unita_amministrative_responsabili?.map((u, index) => {
-              return (
-                <div className="row mb-3" key={index}>
-                  <div className="w-100">
-                    <Card className={'listing-item card-bg border-left-card'}>
-                      <div className="d-flex">
-                        <CardBody className="">
-                          <CardCategory>
-                            <span className="text font-weight-bold">
-                              <UniversalLink href={flattenToAppURL(u['@id'])}>
-                                {u.title || u.id}
-                              </UniversalLink>
-                            </span>
-                          </CardCategory>
-                          <CardText>
-                            {searchResults[u['@id']]?.data?.street}
-                          </CardText>
-                        </CardBody>
-                        {searchResults[u['@id']]?.data?.image && (
-                          <div className="image-container mr-3">
-                            <Image
-                              image={searchResults[u['@id']].data?.image}
-                              alt={searchResults[u['@id'].data?.image_caption]}
-                              title={
-                                searchResults[u['@id'].data?.image_caption]
-                              }
-                            />
-                          </div>
-                        )}
-                      </div>
-                    </Card>
+            {content?.unita_amministrative_responsabili?.length > 0 &&
+              content?.unita_amministrative_responsabili?.map((u, index) => {
+                return (
+                  <div className="row mb-3" key={index}>
+                    <div className="w-100">
+                      <Card className={'listing-item card-bg border-left-card'}>
+                        <div className="d-flex">
+                          <CardBody className="">
+                            <CardCategory>
+                              <span className="text font-weight-bold">
+                                <UniversalLink href={flattenToAppURL(u['@id'])}>
+                                  {u.title || u.id}
+                                </UniversalLink>
+                              </span>
+                            </CardCategory>
+                            <CardText>
+                              {searchResults[u['@id']]?.data?.street}
+                            </CardText>
+                          </CardBody>
+                          {searchResults[u['@id']]?.data?.image && (
+                            <div className="image-container mr-3">
+                              <Image
+                                image={searchResults[u['@id']].data?.image}
+                                alt={
+                                  searchResults[u['@id'].data?.image_caption]
+                                }
+                                title={
+                                  searchResults[u['@id'].data?.image_caption]
+                                }
+                              />
+                            </div>
+                          )}
+                        </div>
+                      </Card>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-          {content?.image ? (
-            <>
-              <Portal
-                node={
-                  __CLIENT__ && document.getElementById('portal-header-image')
-                }
-              >
-                <div>
-                  <Image
-                    image={content.image}
-                    alt={content.caption ?? content.title}
-                    title={content.caption ?? content.title}
-                  />
-                </div>
-              </Portal>
-              <BodyClass className="has-image" />
-            </>
-          ) : (
-            ''
-          )}
+                );
+              })}
+            {content?.image ? (
+              <>
+                <Portal
+                  node={
+                    __CLIENT__ && document.getElementById('portal-header-image')
+                  }
+                >
+                  <div>
+                    <Image
+                      image={content.image}
+                      alt={content.caption ?? content.title}
+                      title={content.caption ?? content.title}
+                    />
+                  </div>
+                </Portal>
+                <BodyClass className="has-image" />
+              </>
+            ) : (
+              ''
+            )}
+          </div>
         </div>
+
+        <TextOrBlocks content={content} />
+
+        <PaginaArgomentoPlaceholderAfterContent content={content} />
       </div>
-
-      <TextOrBlocks content={content} />
-
-      <PaginaArgomentoPlaceholderAfterContent content={content} />
+      <RelatedItems content={content} />
+      <RelatedItemInEvidence content={content} />
     </div>
   ) : (
     <PaginaArgomentoViewNoBlocks content={content} />
