16a33b79b342832cf363991dce8ec924686f2b78
Nicola Zambello <nzambello@protonmail.com>
2021-08-31 15:59:17 +0200

    feat: add subfooter

--- src/components/ItaliaTheme/Footer/FooterSmall.jsx
+++ src/components/ItaliaTheme/Footer/FooterSmall.jsx
@@ -8,6 +8,9 @@ import { UniversalLink } from '@plone/volto/components';
 import { defineMessages, useIntl } from 'react-intl';
 import { Container } from 'design-react-kit/dist/design-react-kit';
 import { getSiteProperty } from '@italia/helpers';
+import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSubFooter, getItemsByPath } from '@italia/addons/volto-subfooter';
 
 const messages = defineMessages({
   goToPage: {
@@ -23,25 +26,45 @@ const messages = defineMessages({
  */
 const FooterSmall = () => {
   const intl = useIntl();
+  const pathname = useLocation().pathname;
+  const dispatch = useDispatch();
   const [links, setLinks] = useState([]);
+
+  const menuItems = useSelector((state) => state.subFooter?.result);
+  const items = getItemsByPath(menuItems, pathname)?.filter(
+    (item) => item.visible,
+  );
+
+  useEffect(() => {
+    dispatch(getSubFooter());
+  }, [dispatch]);
 
   useEffect(() => {
     let _links = getSiteProperty('smallFooterLinks', intl.locale) ?? [];
     setLinks(_links);
   }, [intl.locale]);
 
-  return links.length > 0 ? (
+  return items?.length > 0 || links.length > 0 ? (
     <div className="it-footer-small-prints clearfix">
       <Container tag="div">
         <h3 className="sr-only">{intl.formatMessage(messages.goToPage)}</h3>
         <ul className="it-footer-small-prints-list list-inline mb-0 d-flex flex-column flex-md-row">
-          {links.map((link) => (
-            <li className="list-inline-item" key={link.url}>
-              <UniversalLink href={link.url} title={link.title}>
-                {link.title}
-              </UniversalLink>
-            </li>
-          ))}
+          {items?.length > 0 &&
+            items.map((item) => (
+              <li className="list-inline-item" key={item.url}>
+                <UniversalLink href={item.url} title={item.title}>
+                  {item.title}
+                </UniversalLink>
+              </li>
+            ))}
+          {links?.length > 0 &&
+            links.map((link) => (
+              <li className="list-inline-item" key={link.url}>
+                <UniversalLink href={link.url} title={link.title}>
+                  {link.title}
+                </UniversalLink>
+              </li>
+            ))}
         </ul>
       </Container>
     </div>
