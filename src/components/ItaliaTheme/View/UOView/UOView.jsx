ee23c3e93a11a89a625615a867223315054c99ac
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-10-22 12:06:13 +0200

    fix: fix view persone in uo

--- src/components/ItaliaTheme/View/UOView/UOView.jsx
+++ src/components/ItaliaTheme/View/UOView/UOView.jsx
@@ -141,7 +141,7 @@ const UOView = ({ content }) => {
 
   // create object Ruolo: Persone
   let roles = content?.persone_struttura?.reduce((r, a) => {
-    const role = a.ruolo ?? '';
+    const role = a.ruolo.title ?? '';
     r[role] = r[role] || [];
     r[role].push(a);
     return r;
