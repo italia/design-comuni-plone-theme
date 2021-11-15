03a8345bb25d304e73a12af73daab68f1192a16c
Giulia Ghisini <giulia.ghisini@redturtle.it>
2021-11-15 17:41:40 +0100

    chore: customized Unauthorized view for login spid

---
+++ src/customizations/volto/components/theme/Unauthorized/Unauthorized.jsx
@@ -0,0 +1,18 @@
+/**
+ * @module components/theme/Unauthorized/Unauthorized
+ */
+
+import React from 'react';
+
+import { Unauthorized as UnauthorizedItalia } from '@italia/components/ItaliaTheme';
+
+/**
+ * unauthorized function.
+ * @function Unauthorized
+ * @returns {string} Markup of the unauthorized page.
+ */
+const Unauthorized = () => {
+  return <UnauthorizedItalia />;
+};
+
+export default Unauthorized;
