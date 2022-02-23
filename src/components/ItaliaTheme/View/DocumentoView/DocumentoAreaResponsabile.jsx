d478df706aa1c9645c8f54a5354c3a2ff21f0090
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-02-23 11:10:37 +0100

    chore: refactor Documento view

---
+++ src/components/ItaliaTheme/View/DocumentoView/DocumentoAreaResponsabile.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+
+import {
+  RichTextArticle,
+  OfficeCard,
+} from '@italia/components/ItaliaTheme/View';
+
+const messages = defineMessages({
+  area_responsabile: {
+    id: 'documento_area_responsabile',
+    defaultMessage: 'Area responsabile',
+  },
+});
+
+const DocumentoAreaResponsabile = ({ content }) => {
+  const intl = useIntl();
+
+  return content?.area_responsabile?.length > 0 ? (
+    <RichTextArticle
+      tag_id="area_responsabile"
+      title={intl.formatMessage(messages.area_responsabile)}
+    >
+      <div className="card-wrapper card-teaser-wrapper card-teaser-wrapper-equal">
+        {content.area_responsabile.map((item, i) => (
+          <OfficeCard key={item['@id']} office={item} />
+        ))}
+      </div>
+    </RichTextArticle>
+  ) : (
+    <></>
+  );
+};
+
+export default DocumentoAreaResponsabile;
---
+++ src/components/ItaliaTheme/View/DocumentoView/DocumentoUfficioResponsabile.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+
+import {
+  RichTextArticle,
+  OfficeCard,
+} from '@italia/components/ItaliaTheme/View';
+
+const messages = defineMessages({
+  ufficio_responsabile: {
+    id: 'documento_ufficio_responsabile',
+    defaultMessage: 'Ufficio responsabile',
+  },
+});
+
+const DocumentoUfficioResponsabile = ({ content }) => {
+  const intl = useIntl();
+
+  return content.ufficio_responsabile?.length > 0 ? (
+    <RichTextArticle
+      tag_id="ufficio_responsabile"
+      title={intl.formatMessage(messages.ufficio_responsabile)}
+    >
+      <div className="card-wrapper card-teaser-wrapper card-teaser-wrapper-equal">
+        {content.ufficio_responsabile?.length > 0 && (
+          <>
+            {content.ufficio_responsabile.map((item, i) => (
+              <OfficeCard key={item['@id']} office={item} />
+            ))}
+          </>
+        )}
+      </div>
+    </RichTextArticle>
+  ) : (
+    <></>
+  );
+};
+
+export default DocumentoUfficioResponsabile;
