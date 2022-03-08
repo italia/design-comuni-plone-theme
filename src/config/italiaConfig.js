eaca96a4c8a789612a2fee0099923d8d3bf51755
mamico <mauro.amico@gmail.com>
2022-03-08 15:22:58 +0100

    chore: scrub data for sentry (example)

--- src/config/italiaConfig.js
+++ src/config/italiaConfig.js
@@ -84,6 +84,14 @@ export default function applyConfig(voltoConfig) {
         'Timeout (n)', //errori di recaptcha nella customer satisfaction
         'Uncaught (in promise) Timeout (n)', //errori di recaptcha nella customer satisfaction
       ],
+      // https://docs.sentry.io/platforms/javascript/data-management/sensitive-data/
+      // beforeSend(event) {
+      //   // Modify the event here (e.g.)
+      //   // if (event.user) {
+      //   //   delete event.user.email;
+      //   // }
+      //  return event;
+      // },
     },
     // listBlockTypes: listBlockTypes,
     isMultilingual: false,
