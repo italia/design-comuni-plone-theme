1764a899349820902590814cf0151de5e4c7faab
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-05-13 12:21:09 +0200

    fix: fixed static form fields id and disabled field label style

--- src/customizations/volto-form-block/components/FormView.jsx
+++ src/customizations/volto-form-block/components/FormView.jsx
@@ -145,7 +145,11 @@ const FormView = ({
                             <Field
                               {...field}
                               field_type={field.field_type || 'text'}
-                              name={field.label}
+                              name={
+                                'static_field_' +
+                                (field.field_id ??
+                                  field.name?.toLowerCase()?.replace(' ', ''))
+                              }
                               value={field.value}
                               onChange={() => {}}
                               valid
