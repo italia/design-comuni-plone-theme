44ce31d624f4c028edc4d4e6f75c8b1a8955e22f
Mauro Amico <mauro.amico@gmail.com>
2022-08-01 14:24:53 +0200

    typo

--- src/config/loadables.js
+++ src/config/loadables.js
@@ -1,8 +1,8 @@
 import loadable from '@loadable/component';
-import { loadables as subsitesLoadbles } from './Subsites';
+import { loadables as subsitesLoadables } from './Subsites';
 
 export const loadables = {
   reactSlick: loadable.lib(() => import('react-slick')),
   rrule: loadable.lib(() => import('rrule')),
-  ...subsitesLoadbles,
+  ...subsitesLoadables,
 };
