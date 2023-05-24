3bcf4976cc06e4baa04e6590366b41e386a80916
Wagner Trezub <60133113+Wagner3UB@users.noreply.github.com>
2023-05-24 12:03:05 +0200

    fix: defaultOptions (#175)
    * fix: defaultOptions
    
    * feat i18n and removed import

--- src/config/Blocks/ListingOptions/defaultOptions.js
+++ src/config/Blocks/ListingOptions/defaultOptions.js
@@ -1,6 +1,8 @@
 import { defineMessages } from 'react-intl';
 import { addSchemaField } from '@italia/config/Blocks/ListingOptions';
 
+import config from '@plone/volto/registry';
+
 const messages = defineMessages({
   title: {
     id: 'Titolo',
@@ -18,12 +20,30 @@ const messages = defineMessages({
     id: 'Mostra lo sfondo del blocco',
     defaultMessage: 'Mostra lo sfondo del blocco',
   },
+  show_block_bg: {
+    id: 'Mostra lo sfondo del blocco',
+    defaultMessage: 'Mostra lo sfondo del blocco',
+  },
+  items_color: {
+    id: 'listing_items_color',
+    defaultMessage: "Colore dell'elemento",
+  },
+  bg_color: {
+    id: 'block_bg_color',
+    defaultMessage: 'Colore di sfondo',
+  },
 });
 
 /** DEFAULT **/
 
-const addDefaultAdditionalOptions = (schema, formData, intl, position = 0) => {
+const addDefaultOptions = (schema, formData, intl, position = 0) => {
+  let listing_items_colors =
+    config.blocks.blocksConfig?.listing?.listing_items_colors || [];
+  let listing_bg_colors =
+    config.blocks.blocksConfig?.listing?.listing_bg_colors || [];
+
   let pos = position;
+
   addSchemaField(
     schema,
     'title',
@@ -33,6 +53,19 @@ const addDefaultAdditionalOptions = (schema, formData, intl, position = 0) => {
     pos,
   );
   pos++;
+
+  if (listing_items_colors.length > 0) {
+    addSchemaField(
+      schema,
+      'items_color',
+      intl.formatMessage(messages.items_color),
+      null,
+      { widget: 'color_list', intl: intl, colors: listing_items_colors },
+      pos,
+    );
+    pos++;
+  }
+
   if (
     formData.variation === 'simpleCard' ||
     formData.variation === 'completeBlockLinksTemplate' ||
@@ -62,6 +95,18 @@ const addDefaultAdditionalOptions = (schema, formData, intl, position = 0) => {
   );
   pos++;
 
+  if (listing_bg_colors.length > 0) {
+    addSchemaField(
+      schema,
+      'bg_color',
+      intl.formatMessage(messages.bg_color),
+      null,
+      { widget: 'color_list', intl: intl, colors: listing_bg_colors },
+      pos,
+    );
+    pos++;
+  }
+
   /* if (formData.variation === 'simpleCard') {
     addSchemaField(
       schema,
@@ -72,7 +117,9 @@ const addDefaultAdditionalOptions = (schema, formData, intl, position = 0) => {
       pos,
     );
   } */
+  pos = addDefaultAdditionalOptions(schema, formData, intl, pos);
+
   return pos;
 };
 
-export default addDefaultAdditionalOptions;
+export default addDefaultOptions;
