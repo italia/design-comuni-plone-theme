2052198a119a37e503abca6e17e8b4065aa5cfe9
Piero Nicolli <piero.nicolli@redturtle.it>
2023-06-07 14:56:02 +0200

    chore: fix babel config

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,26 @@
 module.exports = function (api) {
   api.cache(true);
-  const presets = ['razzle'];
+  const presets = [
+    [
+      'razzle',
+      {
+        '@babel/preset-react': { runtime: 'automatic' },
+      },
+    ],
+  ];
   const plugins = [
+    'lodash',
+    '@babel/plugin-proposal-export-default-from', // Stage 1
+    '@babel/plugin-syntax-export-namespace-from', // Stage 4
+    '@babel/plugin-proposal-throw-expressions', // Stage 2
+    '@babel/plugin-proposal-nullish-coalescing-operator', // Stage 4
     [
       'react-intl', // React Intl extractor, required for the whole i18n infrastructure to work
       {
         messagesDir: './build/messages/',
       },
     ],
+    '@loadable/babel-plugin', // Required by the @loadable plugin
   ];
 
   return {
