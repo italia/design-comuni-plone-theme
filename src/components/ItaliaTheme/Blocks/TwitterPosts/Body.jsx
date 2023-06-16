b0b7ebc8a5cc5855c5cbcc094f4b29fa64907d36
Wagner Trezub <60133113+Wagner3UB@users.noreply.github.com>
2023-06-16 10:02:00 +0200

    feat: twitter block errors (#204)
    * feat: twitter block errors remanage
    
    * chore: i18n
    
    * feat: conditional changed
    
    * fix: array condition and i18n

--- src/components/ItaliaTheme/Blocks/TwitterPosts/Body.jsx
+++ src/components/ItaliaTheme/Blocks/TwitterPosts/Body.jsx
@@ -19,6 +19,15 @@ const messages = defineMessages({
     id: 'twitter_posts_no_results',
     defaultMessage: "Non c'è nessun post da mostrare.",
   },
+  not_found: {
+    id: 'twitter_posts_not_found',
+    defaultMessage: 'Post non trovati.',
+  },
+  twitter_error: {
+    id: 'twitter_error',
+    defaultMessage:
+      "Error: Ci scusiamo per l'inconveniente, il server Twitter non risponde.",
+  },
 });
 
 const getTwitterSliderSettings = (nItems) => {
@@ -69,6 +78,7 @@ const Body = ({ data, isEditMode, reactSlick }) => {
 
   const dispatch = useDispatch();
   const request = useSelector((state) => state.twitterPosts);
+
   const authors = data.twitter_accounts?.split(',').filter((a) => a.length > 0);
 
   useEffect(() => {
@@ -94,58 +104,63 @@ const Body = ({ data, isEditMode, reactSlick }) => {
             ))}
           </div>
         )}
+        {!Array.isArray(twitter_posts) && (
+          <div>{intl.formatMessage(messages.not_found)}</div>
+        )}
 
-        <Slider {...getTwitterSliderSettings(twitter_posts.length)}>
-          {twitter_posts.map((tweet, index) => (
-            <div className="it-single-slide-wrapper" key={index}>
-              <div className="tweet rounded">
-                <div className="author">
-                  <div className="user-infos">
-                    <div className="user-name">
-                      <UniversalLink
-                        href={`https://twitter.com/${tweet.author.username}`}
-                      >
-                        {tweet.author.name}
-                      </UniversalLink>
+        {Array.isArray(twitter_posts) && (
+          <Slider {...getTwitterSliderSettings(twitter_posts.length)}>
+            {twitter_posts.map((tweet, index) => (
+              <div className="it-single-slide-wrapper" key={index}>
+                <div className="tweet rounded">
+                  <div className="author">
+                    <div className="user-infos">
+                      <div className="user-name">
+                        <UniversalLink
+                          href={`https://twitter.com/${tweet.author.username}`}
+                        >
+                          {tweet.author.name}
+                        </UniversalLink>
+                      </div>
+                      <div className="user-username pb-3">
+                        <UniversalLink
+                          href={`https://twitter.com/${tweet.author.username}`}
+                        >
+                          @{tweet.author.username}
+                        </UniversalLink>
+                      </div>
                     </div>
-                    <div className="user-username pb-3">
-                      <UniversalLink
-                        href={`https://twitter.com/${tweet.author.username}`}
-                      >
-                        @{tweet.author.username}
-                      </UniversalLink>
+                  </div>
+                  <div
+                    className="tweet-text"
+                    dangerouslySetInnerHTML={{
+                      __html: tweet.text,
+                    }}
+                  />
+                  <div className="date-time">
+                    {moment(tweet.created_at)
+                      .locale(intl.locale)
+                      .format('HH:mm - DD MMM YYYY')}
+                  </div>
+                  <div className="numbers">
+                    <div className="number reply" title="Replies">
+                      <Icon icon="reply" />
+                      {tweet.reply_count > 0 && tweet.reply_count}
                     </div>
-                  </div>
-                </div>
-                <div
-                  className="tweet-text"
-                  dangerouslySetInnerHTML={{
-                    __html: tweet.text,
-                  }}
-                />
-                <div className="date-time">
-                  {moment(tweet.created_at)
-                    .locale(intl.locale)
-                    .format('HH:mm - DD MMM YYYY')}
-                </div>
-                <div className="numbers">
-                  <div className="number reply" title="Replies">
-                    <Icon icon="reply" />
-                    {tweet.reply_count > 0 && tweet.reply_count}
-                  </div>
-                  <div className="number retweet" title="Retweets">
-                    <Icon icon="retweet" />{' '}
-                    {tweet.retweet_count > 0 && tweet.retweet_count}
-                  </div>
-                  <div className="number like" title="Likes">
-                    <Icon icon="heart" />
-                    {tweet.like_count > 0 && tweet.like_count}
+                    <div className="number retweet" title="Retweets">
+                      <Icon icon="retweet" />{' '}
+                      {tweet.retweet_count > 0 && tweet.retweet_count}
+                    </div>
+                    <div className="number like" title="Likes">
+                      <Icon icon="heart" />
+                      {tweet.like_count > 0 && tweet.like_count}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        )}
       </div>
     ) : isEditMode ? (
       intl.formatMessage(messages.no_results)
@@ -153,12 +168,12 @@ const Body = ({ data, isEditMode, reactSlick }) => {
 
   return request?.loadingResults ? (
     <TwitterSkeleton data={data} authors={authors} isEditMode={isEditMode} />
-  ) : request.error ? (
+  ) : request.error && isEditMode ? (
     <Row className="row-full-width">
       <Container className="p-2">
         <div className="pt-4 pb-4">
           <strong>Twitter Posts: </strong>
-          {request.error?.response?.body?.error?.message}
+          {intl.formatMessage(messages.twitter_error)}
         </div>
       </Container>
     </Row>
