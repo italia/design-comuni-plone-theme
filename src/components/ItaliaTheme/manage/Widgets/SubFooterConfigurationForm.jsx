cad966e5a937bfb34fac762f7ecd8936a86e2598
Leonardo Caballero <leonardocaballero@gmail.com>
2023-06-23 10:50:23 -0400

    maintenance: Updated i18n support for SubFooterConfigurationForm manage Widget Block from ItaliaTheme component

--- src/components/ItaliaTheme/manage/Widgets/SubFooterConfigurationForm.jsx
+++ src/components/ItaliaTheme/manage/Widgets/SubFooterConfigurationForm.jsx
@@ -45,6 +45,10 @@ const messages = defineMessages({
     id: 'subfooter-deletemenuitem-button',
     defaultMessage: 'Delete menu item',
   },
+  id_lighthouse: {
+    id: 'ID lighthouse',
+    defaultMessage: 'ID lighthouse',
+  },
   id_lighthouse_description: {
     id: 'ID Lighthouse Help Description',
     defaultMessage:
@@ -109,7 +113,7 @@ const SubFooterConfigurationForm = ({
       />
       <SelectWidget
         id={`${id}-lighthouse`}
-        title="ID lighthouse"
+        title={intl.formatMessage(messages.id_lighthouse)}
         description={intl.formatMessage(messages.id_lighthouse_description)}
         choices={[
           ['accessibility-link', 'accessibility-link'],
