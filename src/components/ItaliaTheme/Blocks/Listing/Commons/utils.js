0bb636ddaaa7e213e643ac04ba4a99a3b654abe3
Martina Bustacchini <41484878+deodorhunter@users.noreply.github.com>
2023-08-22 15:46:05 +0200

    feat: a11y for Slider listing template (#235)
    * feat: a11y for Slider listing template
    
    * fix: styles
    
    * fix: translations
    
    * fix: focus bug on slider autoplay
    
    * fix: trying to work around react-slick not being accessible with infinite and autoplay
    
    * chore: translations
    
    * fix: colors in slider controls
    
    * fix: rework us based on new audit requests
    
    * fix: minor tweaks
    
    ---------
    
    Co-authored-by: Piero Nicolli <piero.nicolli@redturtle.it>

--- src/components/ItaliaTheme/Blocks/Listing/Commons/utils.js
+++ src/components/ItaliaTheme/Blocks/Listing/Commons/utils.js
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from 'react';
+
 export const getCategory = (item, show_type, show_section, props) => {
   let cat = [];
 
@@ -14,4 +16,65 @@ export const getCategory = (item, show_type, show_section, props) => {
     return cat.join(' - ');
   }
   return null;
+};
+export const visibleSlideTitle = (selector) => {
+  // Needed to deal with react-slick duplicating a lot of slides
+  // when used in infinite mode. It's an useless and counterproductive
+  // thing to do on their part, there are multiple issues opened.
+  // The lib is not actually mantained so...
+  return Array.from(document.querySelectorAll(selector)).find((e) => {
+    const rect = e.getBoundingClientRect();
+    return rect.left >= 0 && rect.right <= window.innerWidth;
+  });
+};
+
+export const useSlider = (userAutoplay) => {
+  const slider = useRef(null);
+  const onIntersection = (entries, opt) => {
+    entries.forEach((entry) =>
+      entry.target.classList.toggle('visible', entry.isIntersecting),
+    );
+  };
+  const observer = new IntersectionObserver(onIntersection, {
+    root: null,
+    threshold: 0.5,
+  });
+  if (document.querySelector('.block.listing.slider'))
+    observer.observe(document.querySelector('.block.listing.slider'));
+  useEffect(() => {
+    return () => observer.disconnect();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const focusNext = (currentSlide) => {
+    const sliderElement = document.querySelector('.block.listing.slider');
+    if (!sliderElement) return;
+    const sliderIsVisible = sliderElement.classList.contains('visible');
+
+    if (!sliderIsVisible) {
+      slider.current.slickPause();
+      return;
+    }
+    const slide = sliderElement.querySelectorAll(
+      `a.slide-link[data-slide="${currentSlide}"]`,
+    );
+
+    if ((userAutoplay && !slide) || (userAutoplay && !slide.length > 0)) return;
+
+    // Custom handling of focus as per Arter a11y audit and request
+    const link = visibleSlideTitle(
+      `a.slide-link[data-slide="${currentSlide}"]`,
+    );
+
+    if (!link || document.activeElement === link) {
+      return;
+    }
+    // eslint-disable-next-line no-unused-expressions
+    else link.focus();
+  };
+
+  return {
+    slider,
+    focusNext,
+  };
 };
--- src/components/ItaliaTheme/Blocks/Listing/SliderTemplate.jsx
+++ src/components/ItaliaTheme/Blocks/Listing/SliderTemplate.jsx
@@ -1,20 +1,17 @@
 import 'slick-carousel/slick/slick.css';
 import 'design-comuni-plone-theme/components/slick-carousel/slick/slick-theme.css';
-
 import { Col, Container, Row } from 'design-react-kit';
 import {
   Icon,
   ListingImage,
   ListingLinkMore,
-  NextArrow,
-  PrevArrow,
 } from 'design-comuni-plone-theme/components/ItaliaTheme';
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/interactive-supports-focus */
-import React, { useRef, useState } from 'react';
+import {
+  useSlider,
+  visibleSlideTitle,
+} from 'design-comuni-plone-theme/components/ItaliaTheme/Blocks/Listing/Commons/utils';
+import React, { useState } from 'react';
 import { defineMessages, useIntl } from 'react-intl';
-
 import PropTypes from 'prop-types';
 import { UniversalLink } from '@plone/volto/components';
 import cx from 'classnames';
@@ -22,25 +19,185 @@ import { injectLazyLibs } from '@plone/volto/helpers/Loadable/Loadable';
 
 const messages = defineMessages({
   viewImage: {
-    id: "Vedi l'immagine",
-    defaultMessage: "Vedi l'immagine",
+    id: 'viewImage',
+    defaultMessage:
+      'Sei attualmente in un carosello, per navigare usa le frecce sinistra e destra',
   },
   play: {
     id: 'Play slider',
-    defaultMessage: 'Play',
+    defaultMessage: 'Seleziona per riprodurre',
   },
   pause: {
     id: 'Pause slider',
-    defaultMessage: 'Metti in pausa',
+    defaultMessage: 'Seleziona per mettere in pausa',
+  },
+  precedente: {
+    id: 'precedente',
+    defaultMessage: 'Precedente',
+  },
+  successivo: {
+    id: 'successivo',
+    defaultMessage: 'Successivo',
+  },
+  dots: {
+    id: 'dots',
+    defaultMessage: 'Navigazione elementi slider',
+  },
+  slideDot: {
+    id: 'slideDot',
+    defaultMessage: 'Vai alla slide {index}',
   },
 });
+
+function NextArrow(props) {
+  // Custom handling of focus as per Arter a11y audit and request
+  const { className, style, onClick, intl, currentSlide } = props;
+  const handleClick = (options) => {
+    onClick(options, false);
+  };
+  const handleKeyboardUsers = (e) => {
+    if (e.key === 'Tab' && e.shiftKey) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      const link = visibleSlideTitle(
+        `a.slide-link[data-slide="${currentSlide}"]`,
+      );
+      link && link.focus();
+    }
+  };
+
+  return (
+    <button
+      className={className}
+      style={{ ...style }}
+      onClick={handleClick}
+      title={intl.formatMessage(messages.successivo)}
+      aria-label={intl.formatMessage(messages.successivo)}
+      aria-hidden={true}
+      onKeyDown={handleKeyboardUsers}
+      id="sliderNextArrow"
+    >
+      <Icon icon="chevron-right" key="chevron-right" />
+      <span class="sr-only">{intl.formatMessage(messages.successivo)}</span>
+    </button>
+  );
+}
+
+function PrevArrow(props) {
+  // Custom handling of focus as per Arter a11y audit and request
+  const {
+    className,
+    style,
+    onClick,
+    intl,
+    focusNext,
+    currentSlide,
+    slideCount,
+  } = props;
+  const handleClick = (options) => {
+    onClick(options, false);
+  };
+  const handleKeyboardUsers = (e) => {
+    if (e.key === 'Tab' && !e.shiftKey) {
+      e.stopPropagation();
+      e.preventDefault();
+      if (currentSlide < slideCount) {
+        const link = visibleSlideTitle(
+          `a.slide-link[data-slide="${currentSlide}"]`,
+        );
+        link && link.focus();
+      } else focusNext(0);
+    }
+  };
+  return (
+    <button
+      className={className}
+      style={{ ...style }}
+      onClick={handleClick}
+      title={intl.formatMessage(messages.precedente)}
+      aria-label={intl.formatMessage(messages.precedente)}
+      aria-hidden={true}
+      id="sliderPrevArrow"
+      onKeyDown={handleKeyboardUsers}
+    >
+      <Icon icon="chevron-left" key="chevron-left-prev" />
+      <span class="sr-only">{intl.formatMessage(messages.precedente)}</span>
+    </button>
+  );
+}
+
+const Slide = ({
+  item,
+  index,
+  image,
+  show_image_title,
+  full_width,
+  intl,
+  setUserAutoplay,
+  userAutoplay,
+  slider,
+}) => {
+  const handleKeyboardUsers = (e) => {
+    const { key, shiftKey } = e;
+    if (key === 'Tab') {
+      e.stopPropagation();
+      e.preventDefault();
+      // Keeping auto pause off for now
+      // if (userAutoplay) setUserAutoplay(false);
+      // slider.current.slickPause();
+      let elementToFocus;
+      if (shiftKey) {
+        elementToFocus = document.getElementById('sliderPrevArrow');
+      } else elementToFocus = document.getElementById('sliderNextArrow');
+      elementToFocus.focus();
+    }
+  };
+
+  return (
+    <div
+      className="it-single-slide-wrapper"
+      key={item['@id'] + index}
+      data-slide={index}
+    >
+      <div className="slide-wrapper">
+        {image ? (
+          <figure className="img-wrapper">{image}</figure>
+        ) : (
+          <div className="img-placeholder"></div>
+        )}
+        {show_image_title && (
+          <div className="slide-title">
+            <UniversalLink
+              item={item}
+              title={intl.formatMessage(messages.viewImage)}
+              tabIndex={0}
+              data-slide={index}
+              className={'slide-link no-external-if-link'}
+              onKeyDown={handleKeyboardUsers}
+            >
+              {full_width ? (
+                <Container>
+                  {item.title} <Icon icon="arrow-right" key="arrow-right-fw" />
+                </Container>
+              ) : (
+                <>
+                  {item.title} <Icon icon="arrow-right" key="arrow-right" />
+                </>
+              )}
+            </UniversalLink>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 const SliderTemplate = ({
   items,
   title,
   isEditMode,
   show_block_bg,
-  linkAlign,
   linkTitle,
   linkHref,
   slidesToShow = '1',
@@ -50,15 +207,12 @@ const SliderTemplate = ({
   autoplay = false,
   autoplay_speed = 2, //seconds
   reactSlick,
-  titleLine,
-  linkmore_id_lighthouse,
 }) => {
   const intl = useIntl();
-  const slider = useRef(null);
   const [userAutoplay, setUserAutoplay] = useState(autoplay);
   const nSlidesToShow = parseInt(slidesToShow);
   const Slider = reactSlick.default;
-
+  const { slider, focusNext } = useSlider(userAutoplay);
   const toggleAutoplay = () => {
     if (!slider?.current) return;
     if (userAutoplay) {
@@ -68,6 +222,50 @@ const SliderTemplate = ({
       setUserAutoplay(true);
       slider.current.slickPlay();
     }
+  };
+
+  const renderCustomDots = (props) => {
+    // Custom handling of focus as per Arter a11y audit and request
+    return (
+      <ul
+        className="slick-dots"
+        aria-label={intl.formatMessage(messages.dots)}
+        title={intl.formatMessage(messages.dots)}
+      >
+        {props.map((item, index) => {
+          const El = item.type;
+          const children = item.props.children;
+          // Justified assumption: children is an Object and not an Array here
+          const Child =
+            children.type ||
+            function () {
+              return null;
+            };
+          return (
+            <El
+              className={`${item.props.className} slick-dot`}
+              tabIndex={-1}
+              title={intl.formatMessage(messages.slideDot, {
+                index: index + 1,
+              })}
+              aria-hidden={true}
+            >
+              <Child
+                {...children.props}
+                tabIndex={-1}
+                style={{ padding: 0 }}
+                title={intl.formatMessage(messages.slideDot, {
+                  index: index + 1,
+                })}
+                aria-label={intl.formatMessage(messages.slideDot, {
+                  index: index + 1,
+                })}
+              />
+            </El>
+          );
+        })}
+      </ul>
+    );
   };
 
   const settings = {
@@ -86,8 +284,11 @@ const SliderTemplate = ({
     focusOnSelect: true,
     draggable: true,
     accessibility: true,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow intl={intl} focusNext={focusNext} />,
+    prevArrow: <PrevArrow intl={intl} focusNext={focusNext} />,
+    appendDots: renderCustomDots,
+    // Custom handling of focus as per Arter a11y audit and request
+    afterChange: focusNext,
     responsive: [
       {
         breakpoint: 980,
@@ -99,7 +300,6 @@ const SliderTemplate = ({
     ],
   };
 
-  //const getCaption = (item) => item.description ?? item.rights ?? null;
   return (
     <div
       className={cx(`sliderTemplate slidesToShow-${nSlidesToShow || 1}`, {
@@ -110,9 +310,7 @@ const SliderTemplate = ({
         {title && (
           <Row>
             <Col>
-              <h2 className={cx('mb-4', { 'title-bottom-line': titleLine })}>
-                {title}
-              </h2>
+              <h2 className="mb-4">{title}</h2>
             </Col>
           </Row>
         )}
@@ -126,12 +324,13 @@ const SliderTemplate = ({
             {items?.length > nSlidesToShow && (
               <div className="play-pause-wrapper">
                 <button
-                  onClick={() => toggleAutoplay()}
+                  onClick={toggleAutoplay}
                   title={
                     userAutoplay
                       ? intl.formatMessage(messages.pause)
                       : intl.formatMessage(messages.play)
                   }
+                  tabIndex={0}
                 >
                   <Icon
                     key={userAutoplay ? 'pause' : 'play'}
@@ -150,53 +349,24 @@ const SliderTemplate = ({
                   maxSize: 1600,
                   critical: true,
                 });
-                //if (!image) return null;
                 return (
-                  <div
-                    className="it-single-slide-wrapper"
-                    key={item['@id'] + index}
-                  >
-                    <div className="slide-wrapper">
-                      {image ? (
-                        <figure className="img-wrapper">{image}</figure>
-                      ) : (
-                        <div className="img-placeholder"></div>
-                      )}
-                      {show_image_title && (
-                        <UniversalLink
-                          item={item}
-                          title={intl.formatMessage(messages.viewImage)}
-                          className={'no-external-if-link'}
-                        >
-                          <div className="slide-title">
-                            {full_width ? (
-                              <Container>
-                                {item.title}{' '}
-                                <Icon icon="arrow-right" key="arrow-right-fw" />
-                              </Container>
-                            ) : (
-                              <>
-                                {item.title}{' '}
-                                <Icon icon="arrow-right" key="arrow-right" />
-                              </>
-                            )}
-                          </div>
-                        </UniversalLink>
-                      )}
-                    </div>
-                  </div>
+                  <Slide
+                    image={image}
+                    index={index}
+                    full_width={full_width}
+                    item={item}
+                    show_image_title={show_image_title}
+                    intl={intl}
+                    setUserAutoplay={setUserAutoplay}
+                    userAutoplay={userAutoplay}
+                    slider={slider}
+                  />
                 );
               })}
             </Slider>
           </div>
         </div>
-        <ListingLinkMore
-          title={linkTitle}
-          href={linkHref}
-          linkAlign={linkAlign}
-          className="my-4"
-          linkmoreIdLighthouse={linkmore_id_lighthouse}
-        />
+        <ListingLinkMore title={linkTitle} href={linkHref} className="my-4" />
       </Container>
     </div>
   );
