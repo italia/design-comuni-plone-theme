c4abcd95d40ecc3af56baeb8aa7a06d442b9154d
Wagner Trezub <60133113+Wagner3UB@users.noreply.github.com>
2023-10-23 09:50:04 +0200

    fix: tabnav label in Release log (#373)
    * fix: tabnav label
    
    * fix: object changed

--- src/components/ReleaseLog/ReleaseLog.jsx
+++ src/components/ReleaseLog/ReleaseLog.jsx
@@ -39,12 +39,12 @@ const ReleaseLog = () => {
   }
 
   const LOGS_TO_VIEW = [
-    { name: 'io-comune', file: ReleaseDCPT },
+    { name: 'io-Comune', file: ReleaseDCPT },
     {
-      name: 'io-cittadino',
+      name: 'io-Cittadino',
       file: ReleaseIoCittadino,
     },
-    { name: 'io-prenoto', file: ReleaseIoPrenoto },
+    { name: 'io-Prenoto', file: ReleaseIoPrenoto },
   ];
 
   const [activeTab, toggleTab] = useState(LOGS_TO_VIEW[0].name);
