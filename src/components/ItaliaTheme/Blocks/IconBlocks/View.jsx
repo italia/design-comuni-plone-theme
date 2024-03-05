c7c145626e8125751accaa566eb968034c2cb459
Wagner Trezub <60133113+Wagner3UB@users.noreply.github.com>
2024-03-05 14:55:35 +0100

    fix: condition for showing the title and description for iconBlock (#567)
    * fix: condition for showing the title and description for iconBlock
    
    * fix: release.md
    
    * fix: function changed
    
    ---------
    
    Co-authored-by: Piero Nicolli <pnicolli@users.noreply.github.com>

--- src/components/ItaliaTheme/Blocks/IconBlocks/View.jsx
+++ src/components/ItaliaTheme/Blocks/IconBlocks/View.jsx
@@ -11,6 +11,7 @@ import { Container, Row, Col } from 'design-react-kit';
 import { flattenToAppURL, addAppURL } from '@plone/volto/helpers';
 import { UniversalLink } from '@plone/volto/components';
 import config from '@plone/volto/registry';
+import { checkRedraftHasContent } from 'design-comuni-plone-theme/helpers';
 
 /**
  * View Accordion block class.
@@ -39,7 +40,7 @@ const AccordionView = ({ data, block }) => {
           )}
           <Container className="px-md-4">
             <div className="block-header">
-              {data.title && (
+              {checkRedraftHasContent(data.title) && (
                 <div className="title">
                   {redraft(
                     data.title,
@@ -48,7 +49,7 @@ const AccordionView = ({ data, block }) => {
                   )}
                 </div>
               )}
-              {data.description && (
+              {checkRedraftHasContent(data.description) && (
                 <div className="description">
                   {redraft(
                     data.description,
