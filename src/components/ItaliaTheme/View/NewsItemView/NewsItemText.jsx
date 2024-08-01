3c64496b5e13574756cda57d41f7c2621ea7bdbb
Mauro Amico <mauro.amico@gmail.com>
2024-08-01 16:03:52 +0200

    feat: vista news su ct ComunicatoStampa (#727)
    * chore: nessun impatto diretto sul prodotto, vista news per i prodotti ufficiostampa/comunicatostampa
    
    * feat: usare descrizione_estesa o text nella news (comunicatistampa)
    
    * commenti
    
    * the condition is "the object has the property"
    
    * remove comments
    
    * fix proptypes
    
    * fix: hide section if descrizione_estesa is empty

--- src/components/ItaliaTheme/View/NewsItemView/NewsItemText.jsx
+++ src/components/ItaliaTheme/View/NewsItemView/NewsItemText.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { defineMessages, useIntl } from 'react-intl';
-import { RichTextSection } from 'design-comuni-plone-theme/components/ItaliaTheme/View';
+import {
+  RichText,
+  RichTextSection,
+  richTextHasContent,
+} from 'design-comuni-plone-theme/components/ItaliaTheme/View';
 
 const messages = defineMessages({
   news_item_contenuto: {
@@ -13,20 +17,38 @@ const messages = defineMessages({
 const NewsItemText = ({ content }) => {
   const intl = useIntl();
 
-  return (
-    <RichTextSection
-      data={content.descrizione_estesa}
-      tag_id={'text-body'}
-      field="descrizione_estesa"
-      title={intl.formatMessage(messages.news_item_contenuto)}
-      show_title={false}
-    />
-  );
+  // NewsItem ha la descrizione_estesa (testo con blocchi)
+  // altri CT (come i Comunicati Stampa) potrebbero avere `text`
+  // (richtext senza blocchi).
+  //
+  // La condizione sul campo descrizione_estesa è volutamente semplificata
+  // senza `richTextHasContent` perchè non interessa tanto se il campo
+  // ha o non ha contenuto, ma se il campo esiste o non esiste
+  return Object.hasOwn(content, 'descrizione_estesa')
+    ? richTextHasContent(content.descrizione_estesa) && (
+        <RichTextSection
+          data={content.descrizione_estesa}
+          tag_id={'text-body'}
+          field="descrizione_estesa"
+          title={intl.formatMessage(messages.news_item_contenuto)}
+          show_title={false}
+        />
+      )
+    : content.text && (
+        <RichTextSection
+          tag_id={'text-body'}
+          title={intl.formatMessage(messages.news_item_contenuto)}
+          show_title={false}
+        >
+          <RichText data={content.text} />
+        </RichTextSection>
+      );
 };
 
 NewsItemText.propTypes = {
   content: PropTypes.shape({
     descrizione_estesa: PropTypes.object,
+    text: PropTypes.object,
   }).isRequired,
 };
 
