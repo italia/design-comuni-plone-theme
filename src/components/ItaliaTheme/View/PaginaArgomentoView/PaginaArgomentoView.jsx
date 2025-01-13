889e165003dcfa508ee4c79b95111cd800c52097
Giulia Ghisini <51911425+giuliaghisini@users.noreply.github.com>
2025-01-13 14:27:36 +0100

    fix: uo responsabili image check (#853)

--- src/components/ItaliaTheme/View/PaginaArgomentoView/PaginaArgomentoView.jsx
+++ src/components/ItaliaTheme/View/PaginaArgomentoView/PaginaArgomentoView.jsx
@@ -79,7 +79,7 @@ const PaginaArgomentoView = ({ content }) => {
                   ) {
                     alt = uo_object?.preview_caption;
                   } else if (
-                    uo_object?.image_scales?.image.length > 0 &&
+                    uo_object?.image_scales?.image?.length > 0 &&
                     uo_object?.image_caption
                   ) {
                     alt = uo_object?.image_caption;
