9cf846d864f6c28ee550c80f3b1f98d45839e8bf
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-03-14 10:45:29 +0100

    fix: blockquote render

--- src/config/Slate/Blockquote/index.js
+++ src/config/Slate/Blockquote/index.js
@@ -4,11 +4,13 @@ import BlockquoteMenu from './BlockquoteMenu';
 export default function install(config) {
   const { slate } = config.settings;
 
-  slate.elements.blockquote = ({ children }) => (
-    <blockquote>
-      <p>{children}</p>
-    </blockquote>
-  );
+  slate.elements.blockquote = ({ children, attributes, ...otherProps }) => {
+    return (
+      <blockquote {...attributes}>
+        <p>{children}</p>
+      </blockquote>
+    );
+  };
   slate.buttons.blockquote = (props) => (
     <BlockquoteMenu {...props} title="Blockquote" />
   );
