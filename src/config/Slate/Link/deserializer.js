f7acb30b3513ff5e4c3dbbb16ba924eb2ef486c4
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-08-08 17:12:53 +0200

    fix: slate link deserializer convert <a> without href attribute into simple text (#982)

--- src/config/Slate/Link/deserializer.js
+++ src/config/Slate/Link/deserializer.js
@@ -4,12 +4,16 @@ import { SIMPLELINK } from '@plone/volto-slate/constants';
 
 export const simpleLinkDeserializer = (editor, el) => {
   let parent = el;
-
   let children = Array.from(parent.childNodes)
     .map((el) => deserialize(editor, el))
     .flat();
 
   if (!children.length) children = [{ text: '' }];
+
+  if (el.getAttribute('href') === null) {
+    return jsx('text', {}, children);
+  }
+
   const attrs = {
     type: SIMPLELINK,
     data: {
