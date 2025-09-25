1c10ed3547c3a0aa6544ca5ed20776f91a5b1269
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-09-25 15:06:35 +0200

    fix: improve compatibility when paste an empty href with more children

--- src/config/Slate/Link/deserializer.js
+++ src/config/Slate/Link/deserializer.js
@@ -6,7 +6,7 @@ export const simpleLinkDeserializer = (editor, el) => {
   let parent = el;
   let children = Array.from(parent.childNodes)
     .map((el) => {
-      if (el.removeAttribute) {
+      if (el instanceof Element) {
         el.removeAttribute('style');
       }
       return deserialize(editor, el);
@@ -16,7 +16,7 @@ export const simpleLinkDeserializer = (editor, el) => {
   if (!children.length) children = [{ text: '' }];
 
   if (el.getAttribute('href') === null) {
-    return jsx('text', {}, children);
+    return children;
   }
   const attrs = {
     type: SIMPLELINK,
