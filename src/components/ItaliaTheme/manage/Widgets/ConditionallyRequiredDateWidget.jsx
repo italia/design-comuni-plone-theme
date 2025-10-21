630613bfb56f7c3e2a50ceede57c3466c8870509
sabrina-bongiovanni <116291154+sabrina-bongiovanni@users.noreply.github.com>
2025-10-21 14:51:14 +0200

    fix: tipoIncarico conditions check for data inizio incarico widget (#1021)

--- src/components/ItaliaTheme/manage/Widgets/ConditionallyRequiredDateWidget.jsx
+++ src/components/ItaliaTheme/manage/Widgets/ConditionallyRequiredDateWidget.jsx
@@ -7,8 +7,18 @@ const ConditionallyRequiredDateWidget = (props) => {
   // Check tipo di incarico in formData
   const tipoIncarico = formData?.tipologia_incarico;
 
-  // Make required only if tipo di incarico !== "amministrativo"
-  const isRequired = tipoIncarico !== 'amministrativo';
+  // when selecting tipo di incarico, tipoIncarico is a string
+  // if entering in already existing incarico, the tipoIncarico value will be an object
+  //{
+  //  "title": "Amministrativo",
+  //  "token": "amministrativo"
+  // }
+  // so its necessary to check both conditions
+  // if tipoIncarico.token and tipoIncarico !== 'amministrativo'
+  // the field must not be required
+  const isRequired =
+    tipoIncarico !== 'amministrativo' &&
+    tipoIncarico.token !== 'amministrativo';
 
   return (
     <DatetimeWidget
